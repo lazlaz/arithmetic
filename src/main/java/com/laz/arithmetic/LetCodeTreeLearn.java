46aeb8cfb266f55a16005bd150e39a73a2f448cc
lazlaz <578091472@qq.com>
2019-12-18 10:33:26 +0800

    验证二叉搜索树（中序遍历）

--- src/main/java/com/laz/arithmetic/LetCodeTreeLearn.java
+++ src/main/java/com/laz/arithmetic/LetCodeTreeLearn.java
@@ -1,6 +1,8 @@
 package com.laz.arithmetic;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 import org.junit.Test;
 
@@ -31,6 +33,68 @@ public class LetCodeTreeLearn {
 
 		System.out.println(maxDepth(root));
 
+	}
+
+	// 验证二叉搜索树
+	@Test
+	public void test2() {
+		// TreeNode root = new TreeNode(5);
+		// TreeNode node1 = new TreeNode(1);
+		// TreeNode node2 = new TreeNode(4);
+		// TreeNode node3 = new TreeNode(3);
+		// TreeNode node4 = new TreeNode(6);
+		// root.left = node1;
+		// root.right = node2;
+		//
+		// node2.left = node3;
+		// node2.right = node4;
+
+		TreeNode root = new TreeNode(3);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(5);
+		TreeNode node3 = new TreeNode(0);
+		TreeNode node4 = new TreeNode(2);
+		TreeNode node5 = new TreeNode(4);
+		TreeNode node6 = new TreeNode(6);
+
+		TreeNode node7 = new TreeNode(3);
+		TreeNode node8 = new TreeNode(13);
+		root.left = node1;
+		root.right = node2;
+		node1.left = node3;
+		node1.right = node4;
+		node2.left = node5;
+		node2.right = node6;
+		
+		node4.left = node8;
+		node4.right = node7;
+		System.out.println(isValidBST(root));
+
+	}
+
+	public boolean isValidBST(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+		inorderTraversal(root, list);
+		
+		if (list.size()>0) {
+			int initVal = list.get(0);
+			for (int i=1;i<list.size();i++) {
+				if (initVal>=list.get(i)) {
+					return false;
+				}
+				initVal = list.get(i);
+			}
+		}
+
+		return true;
+	}
+	private void inorderTraversal(TreeNode treeNode, List<Integer> list) {
+		if(treeNode == null) {
+			return;
+		}
+		inorderTraversal(treeNode.left, list);
+		list.add(treeNode.val);
+		inorderTraversal(treeNode.right, list);
 	}
 
 	public int maxDepth(TreeNode root) {
