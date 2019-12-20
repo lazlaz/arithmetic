699cb676319141baca200d3c4c7b5eac37a01a90
lazlaz <578091472@qq.com>
2019-12-20 11:43:14 +0800

    合并两个有序数组

---
+++ src/main/java/com/laz/arithmetic/LetCodeSortLearn.java
@@ -0,0 +1,34 @@
+package com.laz.arithmetic;
+
+import org.junit.Test;
+
+import com.laz.arithmetic.LetCodeTreeLearn.TreeNode;
+
+public class LetCodeSortLearn {
+	// 合并两个有序数组
+		@Test
+		public void test1() {
+			int[] nums1 = new int[]{1,2,3,0,0,0};
+			int[] nums2 = new int[]{2,5,6};
+			merge(nums1,3,nums2,3);
+			for (int num : nums1) {
+				System.out.print(num+" ");
+			}
+		}
+
+		public void merge(int[] nums1, int m, int[] nums2, int n) {
+			for (int i=0;i<nums2.length;i++) {
+				nums1[m+i] = nums2[i];
+			}
+			for (int i=0;i<nums1.length-1;i++) {
+				for (int j=0;j<nums1.length-1;j++) {
+					if(nums1[j]>nums1[j+1])
+	                {
+	                    int temp=nums1[j];
+	                    nums1[j]=nums1[j+1];
+	                    nums1[j+1]=temp;
+	                }
+				}
+			}
+		}
+}
--- src/main/java/com/laz/arithmetic/LetCodeTreeLearn.java
+++ src/main/java/com/laz/arithmetic/LetCodeTreeLearn.java
@@ -1,7 +1,9 @@
 package com.laz.arithmetic;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 import org.junit.Test;
@@ -65,21 +67,125 @@ public class LetCodeTreeLearn {
 		node1.right = node4;
 		node2.left = node5;
 		node2.right = node6;
-		
+
 		node4.left = node8;
 		node4.right = node7;
 		System.out.println(isValidBST(root));
 
 	}
 
+	// 对称二叉树
+	@Test
+	public void test3() {
+
+		TreeNode root = new TreeNode(3);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(5);
+		TreeNode node3 = new TreeNode(0);
+		TreeNode node4 = new TreeNode(2);
+		TreeNode node5 = new TreeNode(4);
+		TreeNode node6 = new TreeNode(6);
+
+		TreeNode node7 = new TreeNode(3);
+		TreeNode node8 = new TreeNode(13);
+		root.left = node1;
+		root.right = node2;
+		node1.left = node3;
+		node1.right = node4;
+		node2.left = node5;
+		node2.right = node6;
+
+		node4.left = node8;
+		node4.right = node7;
+		System.out.println(isSymmetric(root));
+
+	}
+
+	// 二叉树的层次遍历
+	@Test
+	public void test4() {
+
+		TreeNode root = new TreeNode(3);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(5);
+		TreeNode node3 = new TreeNode(0);
+		TreeNode node4 = new TreeNode(2);
+		TreeNode node5 = new TreeNode(4);
+		TreeNode node6 = new TreeNode(6);
+
+		TreeNode node7 = new TreeNode(3);
+		TreeNode node8 = new TreeNode(13);
+		root.left = node1;
+		root.right = node2;
+		node1.left = node3;
+		node1.right = node4;
+		node2.left = node5;
+		node2.right = node6;
+
+		node4.left = node8;
+		node4.right = node7;
+		System.out.println(levelOrder(root));
+
+	}
+
+	
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> levels = new ArrayList<List<Integer>>();
+		if (root == null)
+			return levels;
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		int level = 0;
+		while (!queue.isEmpty()) {
+			levels.add(new ArrayList<Integer>());
+			int level_length = queue.size();
+			for (int i = 0; i < level_length; ++i) {
+				TreeNode node = queue.remove();
+				levels.get(level).add(node.val);
+				if (node.left != null)
+					queue.add(node.left);
+				if (node.right != null)
+					queue.add(node.right);
+			}
+			level++;
+		}
+		return levels;
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+		List<TreeNode> list = new ArrayList<>();
+		inorderTraversal2(root, list);
+
+		if (list.size() > 0) {
+			int mid = list.size() / 2;
+			if (list.get(mid) == root) {
+				return true;
+			}
+			return false;
+		}
+
+		return true;
+	}
+
+	private void inorderTraversal2(TreeNode treeNode, List<TreeNode> list) {
+		if (treeNode == null) {
+			return;
+		}
+		inorderTraversal2(treeNode.left, list);
+		list.add(treeNode);
+		inorderTraversal2(treeNode.right, list);
+
+	}
+
 	public boolean isValidBST(TreeNode root) {
 		List<Integer> list = new ArrayList<>();
 		inorderTraversal(root, list);
-		
-		if (list.size()>0) {
+
+		if (list.size() > 0) {
 			int initVal = list.get(0);
-			for (int i=1;i<list.size();i++) {
-				if (initVal>=list.get(i)) {
+			for (int i = 1; i < list.size(); i++) {
+				if (initVal >= list.get(i)) {
 					return false;
 				}
 				initVal = list.get(i);
@@ -88,8 +194,9 @@ public class LetCodeTreeLearn {
 
 		return true;
 	}
+
 	private void inorderTraversal(TreeNode treeNode, List<Integer> list) {
-		if(treeNode == null) {
+		if (treeNode == null) {
 			return;
 		}
 		inorderTraversal(treeNode.left, list);
