bf60a2b9379def4a551385fa08c9ea870d7777d3
liuzhai <578091472@qq.com>
2020-04-09 17:35:56 +0800

    增加括号生成

--- src/main/java/com/laz/arithmetic/LeetCode3.java
+++ src/main/java/com/laz/arithmetic/LeetCode3.java
@@ -291,4 +291,85 @@ public class LeetCode3 {
 
 	}
 
+	// 括号生成
+	@Test
+	public void test8() {
+		List<String> list = generateParenthesis(2);
+		for (String str : list) {
+			System.out.println(str);
+		}
+	}
+	   // 做减法
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        // 特判
+        if (n == 0) {
+            return res;
+        }
+
+        // 执行深度优先遍历，搜索可能的结果
+        dfs("", n, n, res);
+        return res;
+    }
+
+    /**
+     * @param curStr 当前递归得到的结果
+     * @param left   左括号还有几个可以使用
+     * @param right  右括号还有几个可以使用
+     * @param res    结果集
+     */
+    private void dfs(String curStr, int left, int right, List<String> res) {
+        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+        // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+
+        // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+        if (left > right) {
+            return;
+        }
+
+        if (left > 0) {
+            dfs(curStr + "(", left - 1, right, res);
+        }
+
+        if (right > 0) {
+            dfs(curStr + ")", left, right - 1, res);
+        }
+    }
+
+	public List<String> generateParenthesis2(int n) {
+		List<String> combinations = new ArrayList();
+		generateAll(new char[2 * n], 0, combinations);
+		return combinations;
+	}
+
+	public void generateAll(char[] current, int pos, List<String> result) {
+		if (pos == current.length) {
+			if (valid(current))
+				result.add(new String(current));
+		} else {
+			current[pos] = '(';
+			generateAll(current, pos + 1, result);
+			current[pos] = ')';
+			generateAll(current, pos + 1, result);
+		}
+	}
+
+	public boolean valid(char[] current) {
+		int balance = 0;
+		for (char c : current) {
+			if (c == '(')
+				balance++;
+			else
+				balance--;
+			if (balance < 0)
+				return false;
+		}
+		return (balance == 0);
+	}
+
 }
