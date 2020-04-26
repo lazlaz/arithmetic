02a58ac02d8aeb97e1fd8bd56639e3a970cf191a
liuzhai <578091472@qq.com>
2020-04-26 17:30:56 +0800

    单词规律。丑数

---
+++ src/main/java/com/laz/arithmetic/LeetCode4.java
@@ -0,0 +1,83 @@
+package com.laz.arithmetic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class LeetCode4 {
+	//丑数
+	@Test
+	public void test1() {
+		System.out.println(isUgly(8));
+	}
+	 public boolean isUgly(int num) {
+		 if (num == 0) {
+			 return false;
+		 }
+		 if (num == 1) {
+			 return true;
+		 }
+		 while (num%2 == 0 || num%3 == 0 || num%5 == 0) {
+			 if (num%2 == 0) {
+				 num=num/2;
+			 } else if (num%3 == 0) {
+				 num=num/3;
+			 } else if (num%5 == 0) {
+				 num=num/5;
+			 }
+		 }
+		 if (num == 1) {
+			 return true;
+		 }
+		 return false;
+	 }
+
+	//单词规律
+	@Test
+	public void test2() {
+		System.out.println(wordPattern("","beef"));
+	}
+	public boolean wordPattern(String pattern, String str) {
+	       String[] p =  pattern.split("");
+	       String[] s =  str.split(" ");
+	       
+	       if (pattern.equals("") && !str.equals("")) {
+	    	   return false;
+	       }
+	       if (!pattern.equals("") && str.equals("")) {
+	    	   return false;
+	       }
+	       if (p.length != s.length) {
+	    	   return false;
+	       }
+	       Map<String,Integer> pMap = new HashMap<String,Integer>();
+	       Map<String,Integer> sMap = new HashMap<String,Integer>();
+	       int pNum = 0;
+	       int sNum = 0;
+	       String pString = "";
+	       String sString = "";
+	       for (String p1:p) {
+	    	   if (pMap.get(p1) != null) {
+	    		   pString += pMap.get(p1);
+	    	   } else {
+	    		   pString += pNum;
+	    		   pMap.put(p1,pNum);
+	    		   pNum++;
+	    	   }
+	       }
+	       for (String s1:s) {
+	    	   if (sMap.get(s1) != null) {
+	    		   sString += sMap.get(s1);
+	    	   } else {
+	    		   sString += sNum;
+	    		   sMap.put(s1,sNum);
+	    		   sNum++;
+	    	   }
+	       }
+	       if (pString.equals(sString)) {
+	           return true;
+	       }
+	       return false;
+	}
+}
