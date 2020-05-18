e44d0e92c0ecb6f37f8876265702d1688d5830c4
liuzhai <578091472@qq.com>
2020-05-18 11:44:01 +0800

    乘积最大子数组

--- src/main/java/com/laz/arithmetic/LeetCode3.java
+++ src/main/java/com/laz/arithmetic/LeetCode3.java
@@ -747,4 +747,27 @@ public class LeetCode3 {
 		}
 		return dp[nums.length-1];
 	}
+	
+	//乘积最大子数组
+	@Test
+	public void test17() {
+		int[] nums = new int[] {-2,0,-1,-1};
+		System.out.println(maxProduct(nums));
+	}
+	public int maxProduct(int[] nums) {
+		int dpMax = 1;
+		int dpMin = 1;
+		int max = Integer.MIN_VALUE;
+		for (int i=0;i<nums.length;i++) {
+			 if(nums[i] < 0){ 
+              int tmp = dpMax;
+              dpMax = dpMin;
+              dpMin = tmp;
+	        }
+			dpMax = Math.max(dpMax*nums[i], nums[i]);
+			dpMin = Math.min(dpMin*nums[i], nums[i]);
+			max = Math.max(dpMax, max);
+		}
+		return max;
+    }
 }
