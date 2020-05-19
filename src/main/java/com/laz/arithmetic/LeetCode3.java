b1f0d68535162f0eab5f84302de82c59d60cf8f1
liuzhai <578091472@qq.com>
2020-05-19 14:31:33 +0800

    验证回文字符串 Ⅱ

--- src/main/java/com/laz/arithmetic/LeetCode3.java
+++ src/main/java/com/laz/arithmetic/LeetCode3.java
@@ -770,4 +770,39 @@ public class LeetCode3 {
 		}
 		return max;
     }
+	
+	//验证回文字符串 Ⅱ
+	@Test
+	public void test18() {
+		System.out.println(validPalindrome("eedede"));
+	}
+	
+	public boolean validPalindrome(String s) {
+		int low = 0, high=s.length()-1;
+		while (low<high) {
+			char c1 = s.charAt(low),c2 = s.charAt(high);
+			if (c1 == c2) {
+				low++;
+				high--;
+			}else {
+                boolean flag1 = true, flag2 = true;
+                for (int i = low, j = high - 1; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag1 = false;
+                        break;
+                    }
+                }
+                for (int i = low + 1, j = high; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag2 = false;
+                        break;
+                    }
+                }
+                return flag1 || flag2;
+            }
+		}
+		return true;
+	}
 }
