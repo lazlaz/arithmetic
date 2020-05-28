4ade57ec1c5377f31a326e95e900292fa2634937
liuzhai <578091472@qq.com>
2020-05-28 10:49:41 +0800

    修复

--- src/main/java/com/laz/arithmetic/LeetCode3.java
+++ src/main/java/com/laz/arithmetic/LeetCode3.java
@@ -840,4 +840,42 @@ public class LeetCode3 {
         }
         return left;
     }
+	
+	//和可被 K 整除的子数组
+	@Test
+	public void test20() {
+		int[] A = new int[] { 4,5,0,-2,-3,1};
+		System.out.println(subarraysDivByK(A,5));
+	}
+	//超时
+	 public int subarraysDivByK2(int[] A, int K) {
+		 int value = 0;
+		 for (int i=0;i<A.length;i++) {
+			 int count = 0;
+			 for (int j=i;j<A.length;j++) {
+				 count+=A[j];
+				 if (count%K==0) {
+					 System.out.println(count);
+					 value++;
+				 }
+			 }
+		 }
+		 return value;
+	 }
+	 
+	//前缀和
+	 public int subarraysDivByK(int[] A, int K) {
+	        Map<Integer, Integer> record = new HashMap<>();
+	        record.put(0, 1);
+	        int sum = 0, ans = 0;
+	        for (int elem: A) {
+	            sum += elem;
+	            // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
+	            int modulus = (sum % K + K) % K;
+	            int same = record.getOrDefault(modulus, 0);
+	            ans += same;
+	            record.put(modulus, same + 1);
+	        }
+	        return ans;
+	    }
 }
