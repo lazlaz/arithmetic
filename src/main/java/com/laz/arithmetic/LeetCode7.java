2bf1833b269866eea65ba49669271c38ec86598e
zhouxingting <zxtzxt140420>
2020-07-06 21:30:56 +0800

    不同路径 II

--- src/main/java/com/laz/arithmetic/LeetCode7.java
+++ src/main/java/com/laz/arithmetic/LeetCode7.java
@@ -564,5 +564,39 @@ public class LeetCode7 {
 
 		}
 	}
-
+	
+	//不同路径 II
+	@Test
+	public void test16() {
+		Assert.assertEquals(2, uniquePathsWithObstacles(new int[][] {{
+			0,0,0
+		},{
+			0,1,0
+		},{
+			0,0,0
+		}}));
+	}
+	 public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		 if (obstacleGrid[0][0] == 1) {
+			 return 0;
+		 }
+		 int m = obstacleGrid.length;
+		 int n = obstacleGrid[0].length;
+		 int[][] dp = new int[m][n];
+		 dp[0][0] = 1;
+		 for (int i=1;i<m;i++) {
+			 //第一列，有1后为0
+			 dp[i][0] = obstacleGrid[i][0] == 1 || dp[i - 1][0] == 0 ? 0 : 1;
+		 }
+		 for (int i=1;i<n;i++) {
+			 //第一行，有1后为0
+			 dp[0][i] = obstacleGrid[0][i] == 1 || dp[0][i - 1] == 0 ? 0 : 1;;
+		 }
+		 for (int i=1;i<m;i++) {
+			 for (int j=1;j<n;j++) {
+				 dp[i][j] = obstacleGrid[i][j] == 1 ?0: dp[i - 1][j] + dp[i][j - 1];
+			 }
+		 }
+		 return dp[m-1][n-1];
+	 }
 }
