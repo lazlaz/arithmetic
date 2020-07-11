a087268479db9ca11153027aa54ec9d5ad5ec586
zhouxingting <zxtzxt140420>
2020-07-11 20:12:16 +0800

    计算右侧小于当前元素的个数（树状数组）

---
+++ src/main/java/com/laz/arithmetic/LeetCode8.java
@@ -0,0 +1,104 @@
+package com.laz.arithmetic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.google.common.base.Joiner;
+
+/**
+ * 
+ *
+ */
+public class LeetCode8 {
+	// 计算右侧小于当前元素的个数
+	@Test
+	public void test1() {
+		int[] nums = new int[] { 5, 2, 6, 1 };
+		List<Integer> res = countSmaller(nums);
+		System.out.println(Joiner.on(",").join(res));
+	}
+	private int[] c;
+    private int[] a;
+    //树壮数组+离散化
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> resultList = new ArrayList<Integer>(); 
+        discretization(nums);
+        init(nums.length + 5);
+        for (int i = nums.length - 1; i >= 0; --i) {
+            int id = getId(nums[i]);
+            resultList.add(query(id - 1));
+            update(id,1);
+        }
+        Collections.reverse(resultList);
+        return resultList;
+    }
+
+    private void init(int length) {
+        c = new int[length];
+        Arrays.fill(c, 0);
+    }
+
+    private int lowBit(int x) {
+        return x & (-x);
+    }
+
+    private void update(int pos,int k) {
+        while (pos < c.length) {
+            c[pos] += k;
+            pos += lowBit(pos);
+        }
+    }
+
+    //查询sum[i]= C[i] + C[i-2k1] + C[(i - 2k1) - 2k2] + .....； 前缀和
+    private int query(int pos) {
+        int ret = 0;
+        while (pos > 0) {
+            ret += c[pos];
+            pos -= lowBit(pos);
+        }
+
+        return ret;
+    }
+
+    private void discretization(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int size = set.size();
+        a = new int[size];
+        int index = 0;
+        for (int num : set) {
+            a[index++] = num;
+        }
+        Arrays.sort(a);
+    }
+
+    private int getId(int x) {
+        return Arrays.binarySearch(a, x) + 1;
+    }
+	
+	//暴力解法超时
+	public List<Integer> countSmaller2(int[] nums) {
+		List<Integer> res = new ArrayList<Integer>();
+		if (nums == null) {
+			return res;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			int count = 0;
+			for (int j = i; j < nums.length; j++) {
+				if (nums[j]<nums[i]) {
+					count++;
+				}
+			}
+			res.add(count);
+		}
+		return res;
+	}
+}
