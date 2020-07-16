ffff7ff7b65aca7e58bef0ea91312c58e367775d
zhouxingting <zxtzxt140420>
2020-07-16 21:16:08 +0800

    判断二分图 dfs bfs两种解法

--- src/main/java/com/laz/arithmetic/LeetCode8.java
+++ src/main/java/com/laz/arithmetic/LeetCode8.java
@@ -8,6 +8,7 @@ import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 import org.junit.Assert;
@@ -487,5 +488,81 @@ public class LeetCode8 {
 			// 撤销操作
 			Collections.swap(output, first, i);
 		}
+	}
+
+	// 判断二分图
+	@Test
+	public void test11() {
+		int[][] graph = new int[][] {
+			{1,3},
+			{0,2},
+			{1,3},
+			{0,2}
+		};
+		Assert.assertEquals(true, new Solution().isBipartite(graph));
+	}
+
+	class Solution {
+		private static final int UNCOLORED = 0;
+		private static final int RED = 1;
+		private static final int GREEN = 2;
+		private int[] color;
+		private boolean valid;
+
+		public boolean isBipartite(int[][] graph) {
+			int n = graph.length;
+			valid = true;
+			color = new int[n];
+			Arrays.fill(color, UNCOLORED);
+			for (int i=0;i<n&&valid;i++) {
+				if (color[i] == UNCOLORED) {
+					dfs(i,RED,graph);
+				}
+			}
+			return valid;
+		}
+		public void dfs(int node,int c,int[][] graph) {
+			color[node] = c; //改节点为一个色
+			int cNei = c == RED ?GREEN:RED; //其他类节点为相反色
+			//遍历该节点相连的点
+			for (int neighbor:graph[node]) {
+				if (color[neighbor] == UNCOLORED) {
+					dfs(neighbor,cNei,graph);
+					if (!valid) {
+						return;
+					}
+				} else if (color[neighbor] != cNei) {
+					valid = false;
+					return;
+				}
+			}
+		}
+		
+		//广度遍历解法
+		public boolean isBipartite2(int[][] graph) {
+	        int n = graph.length;
+	        color = new int[n];
+	        Arrays.fill(color, UNCOLORED);
+	        for (int i = 0; i < n; ++i) {
+	            if (color[i] == UNCOLORED) {
+	                Queue<Integer> queue = new LinkedList<Integer>();
+	                queue.offer(i);
+	                color[i] = RED;
+	                while (!queue.isEmpty()) {
+	                    int node = queue.poll();
+	                    int cNei = color[node] == RED ? GREEN : RED;
+	                    for (int neighbor : graph[node]) {
+	                        if (color[neighbor] == UNCOLORED) {
+	                            queue.offer(neighbor);
+	                            color[neighbor] = cNei;
+	                        } else if (color[neighbor] != cNei) {
+	                            return false;
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        return true;
+	    }
 	}
 }
