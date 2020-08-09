a95f9cef17894996547919eada2b736b78e5ad86
laz <laz>
2020-08-09 10:51:18 +0800

    复原IP地址

--- src/main/java/com/laz/arithmetic/LeetCode10.java
+++ src/main/java/com/laz/arithmetic/LeetCode10.java
@@ -336,4 +336,62 @@ public class LeetCode10 {
 		y.val = tmp;
 	}
 
+	// 复原IP地址
+	@Test
+	public void test8() {
+		List<String> ret = new Solution8().restoreIpAddresses("25525511135");
+		System.out.println(Joiner.on(",").join(ret));
+	}
+
+	class Solution8 {
+		final int SEG_COUNT = 4;
+		List<String> ans = new ArrayList<String>();
+		int[] segments = new int[SEG_COUNT];
+
+		public List<String> restoreIpAddresses(String s) {
+			segments = new int[SEG_COUNT];
+			backtrack(s, 0, 0);
+			return ans;
+		}
+
+		private void backtrack(String s, int segId, int segStart) {
+			// 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+			if (segId == SEG_COUNT) {
+				if (segStart == s.length()) {
+					StringBuffer ipAddr = new StringBuffer();
+					for (int i = 0; i < SEG_COUNT; ++i) {
+						ipAddr.append(segments[i]);
+						if (i != SEG_COUNT - 1) {
+							ipAddr.append('.');
+						}
+					}
+					ans.add(ipAddr.toString());
+				}
+				//如果有4个IP了，但是字符串还有剩，则提前回溯，改ip不符合
+				return;
+			}
+			// 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+			if (segStart == s.length()) {
+				return;
+			}
+			// 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+			if (s.charAt(segStart) == '0') {
+				segments[segId] = 0;
+				backtrack(s, segId + 1, segStart + 1);
+			}
+			// 一般情况，枚举每一种可能性并递归
+			int addr = 0;
+			for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+				addr = addr * 10 + (s.charAt(segEnd) - '0');
+				// 地址需要大于0且小于255才有效
+				if (addr > 0 && addr <= 0xFF) {
+					segments[segId] = addr;
+					backtrack(s, segId + 1, segEnd + 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+
 }
