92bec9bb04cba41f4fa556e2473f96cab2d653a9
laz <laz>
2020-08-10 21:25:03 +0800

    删除排序链表中的重复元素 II

--- src/main/java/com/laz/arithmetic/LeetCode10.java
+++ src/main/java/com/laz/arithmetic/LeetCode10.java
@@ -469,4 +469,30 @@ public class LeetCode10 {
 		}
 		return ans;
 	}
+
+	// 删除排序链表中的重复元素 II
+	@Test
+	public void test11() {
+		ListNode head = Utils.createListNode(new Integer[] { 1, 2, 3, 3, 4, 4, 5 });
+		ListNode head2 = deleteDuplicates(head);
+		Utils.printListNode(head2);
+	}
+
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null)
+			return head; // 若head为空则直接返回null
+		ListNode dummy = new ListNode(-1); // 建立一个虚拟头结点
+		ListNode tail = dummy; // 定义一个尾巴，用于尾插法。
+		for (ListNode l = head, r = head; l != null; l = r) {
+			while (r != null && r.val == l.val) {
+				r = r.next; // 只要r不为空并且与l的值相等则一直向后移动
+			}
+			if (l.next == r) { // 若长度为1，则通过尾插法加入。
+				tail.next = l; // 基本的尾插法
+				tail = l;
+				tail.next = null; // 这里记得将尾部的后面置为null，不然可能后面会带着一些其他的节点。
+			}
+		}
+		return dummy.next;
+	}
 }
