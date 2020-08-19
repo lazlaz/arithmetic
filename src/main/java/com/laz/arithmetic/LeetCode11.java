1750cdea705c46c32fdc490e040e51b56d228421
liuzhai <578091472@qq.com>
2020-08-19 20:55:01 +0800

    回文子串

--- src/main/java/com/laz/arithmetic/LeetCode11.java
+++ src/main/java/com/laz/arithmetic/LeetCode11.java
@@ -78,4 +78,26 @@ public class LeetCode11 {
 			ret.add(new ArrayList<String>(list));
 		}
 	}
+
+	@Test
+	// 回文子串
+	public void test2() {
+		Assert.assertEquals(6, new Solution2().countSubstrings("aaa"));
+		Assert.assertEquals(3, new Solution2().countSubstrings("abc"));
+	}
+
+	class Solution2 {
+		public int countSubstrings(String s) {
+			int n = s.length(), ans = 0;
+			for (int i = 0; i < 2 * n - 1; ++i) {
+				int l = i / 2, r = i / 2 + i % 2;
+				while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+					--l;
+					++r;
+					++ans;
+				}
+			}
+			return ans;
+		}
+	}
 }
