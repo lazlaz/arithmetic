29aeccc5b8c279a57e81b38d398590794d7fb60d
laz <laz>
2020-08-26 22:14:29 +0800

    最长上升子序列

--- src/main/java/com/laz/arithmetic/LeetCode11.java
+++ src/main/java/com/laz/arithmetic/LeetCode11.java
@@ -443,4 +443,36 @@ public class LeetCode11 {
 			dfs(cur + 1, last, nums);
 		}
 	}
+
+	// 最长上升子序列
+	@Test
+	public void test11() {
+		Assert.assertEquals(4, new Solution11().lengthOfLIS(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 }));
+		Assert.assertEquals(1, new Solution11().lengthOfLIS(new int[] { 2, 2 }));
+	}
+
+	class Solution11 {
+		public int lengthOfLIS(int[] nums) {
+			if (nums.length == 0) {
+				return 0;
+			}
+			// dp[i]dp[i] 为考虑前 ii 个元素，以第 ii 个数字结尾的最长上升子序列的长度
+			int[] dp = new int[nums.length];
+			dp[0] = 1;
+			int maxans = 1;
+			for (int i = 1; i < dp.length; i++) {
+				int maxval = 0;
+				for (int j = 0; j < i; j++) {
+					if (nums[i] > nums[j]) {
+						maxval = Math.max(maxval, dp[j]);
+					}
+				}
+				dp[i] = maxval + 1;
+				maxans = Math.max(maxans, dp[i]);
+			}
+			return maxans;
+		}
+
+	}
+
 }
