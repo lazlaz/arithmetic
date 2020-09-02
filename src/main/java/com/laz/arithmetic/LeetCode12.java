7be1e30e2f399acbe49a6f685d9cfb6b8f2098e1
liuzhai <578091472@qq.com>
2020-09-02 19:15:41 +0800

    表示数值的字符串（有限状态机）

--- src/main/java/com/laz/arithmetic/LeetCode12.java
+++ src/main/java/com/laz/arithmetic/LeetCode12.java
@@ -1,39 +1,160 @@
 package com.laz.arithmetic;
 
-import org.junit.Test;
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class LeetCode12 {
 	// 超级次方
 	@Test
 	public void test1() {
-		Assert.assertEquals(1024, superPow(2, new int[] { 1,0 }));
+		Assert.assertEquals(1024, superPow(2, new int[] { 1, 0 }));
 	}
+
 	int base = 1337;
+
 	int mypow(int a, int k) {
-		//(a * b) % k = (a % k)(b % k) % k
-		if (k == 0) return 1;
-	    a %= base;
-	    int res = 1;
-	    for (int i = 0; i < k; i++) {
-	        // 这里有乘法，是潜在的溢出点
-	        res *= a;
-	        // 对乘法结果求模
-	        res %= base;
-	    }
-	    return res;
-	}
-	public int superPow(int a, int[] b) {
-		if (b.length==0)
+		// (a * b) % k = (a % k)(b % k) % k
+		if (k == 0)
 			return 1;
-		int last = b[b.length-1];
-		int[] newB = Arrays.copyOf(b, b.length-1);
+		a %= base;
+		int res = 1;
+		for (int i = 0; i < k; i++) {
+			// 这里有乘法，是潜在的溢出点
+			res *= a;
+			// 对乘法结果求模
+			res %= base;
+		}
+		return res;
+	}
+
+	public int superPow(int a, int[] b) {
+		if (b.length == 0)
+			return 1;
+		int last = b[b.length - 1];
+		int[] newB = Arrays.copyOf(b, b.length - 1);
 		int part1 = mypow(a, last);
 		int part2 = mypow(superPow(a, newB), 10);
 		// 每次乘法都要求模
 		return (part1 * part2) % base;
 	}
+
+	// 表示数值的字符串
+	@Test
+	public void test2() {
+		Assert.assertEquals(false, isNumber("1a3.14"));
+		Assert.assertEquals(true, isNumber("-1E-16"));
+		Assert.assertEquals(false, isNumber("E3"));
+	}
+	//有限状态机
+	public boolean isNumber(String s) {
+        Map<State, Map<CharType, State>> transfer = new HashMap<State, Map<CharType, State>>();
+        Map<CharType, State> initialMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_SPACE, State.STATE_INITIAL);
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+            put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
+        }};
+        transfer.put(State.STATE_INITIAL, initialMap);
+        Map<CharType, State> intSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+        }};
+        transfer.put(State.STATE_INT_SIGN, intSignMap);
+        Map<CharType, State> integerMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+            put(CharType.CHAR_POINT, State.STATE_POINT);
+            put(CharType.CHAR_SPACE, State.STATE_END);
+        }};
+        transfer.put(State.STATE_INTEGER, integerMap);
+        Map<CharType, State> pointMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+            put(CharType.CHAR_SPACE, State.STATE_END);
+        }};
+        transfer.put(State.STATE_POINT, pointMap);
+        Map<CharType, State> pointWithoutIntMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+        }};
+        transfer.put(State.STATE_POINT_WITHOUT_INT, pointWithoutIntMap);
+        Map<CharType, State> fractionMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+            put(CharType.CHAR_SPACE, State.STATE_END);
+        }};
+        transfer.put(State.STATE_FRACTION, fractionMap);
+        Map<CharType, State> expMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+            put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
+        }};
+        transfer.put(State.STATE_EXP, expMap);
+        Map<CharType, State> expSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }};
+        transfer.put(State.STATE_EXP_SIGN, expSignMap);
+        Map<CharType, State> expNumberMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+            put(CharType.CHAR_SPACE, State.STATE_END);
+        }};
+        transfer.put(State.STATE_EXP_NUMBER, expNumberMap);
+        Map<CharType, State> endMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_SPACE, State.STATE_END);
+        }};
+        transfer.put(State.STATE_END, endMap);
+
+        int length = s.length();
+        State state = State.STATE_INITIAL;
+
+        for (int i = 0; i < length; i++) {
+            CharType type = toCharType(s.charAt(i));
+            if (!transfer.get(state).containsKey(type)) {
+                return false;
+            } else {
+                state = transfer.get(state).get(type);
+            }
+        }
+        return state == State.STATE_INTEGER || state == State.STATE_POINT || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER || state == State.STATE_END;
+    }
+
+    public CharType toCharType(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return CharType.CHAR_NUMBER;
+        } else if (ch == 'e' || ch == 'E') {
+            return CharType.CHAR_EXP;
+        } else if (ch == '.') {
+            return CharType.CHAR_POINT;
+        } else if (ch == '+' || ch == '-') {
+            return CharType.CHAR_SIGN;
+        } else if (ch == ' ') {
+            return CharType.CHAR_SPACE;
+        } else {
+            return CharType.CHAR_ILLEGAL;
+        }
+    }
+
+    enum State {
+        STATE_INITIAL,
+        STATE_INT_SIGN,
+        STATE_INTEGER,
+        STATE_POINT,
+        STATE_POINT_WITHOUT_INT,
+        STATE_FRACTION,
+        STATE_EXP,
+        STATE_EXP_SIGN,
+        STATE_EXP_NUMBER,
+        STATE_END,
+    }
+
+    enum CharType {
+        CHAR_NUMBER,
+        CHAR_EXP,
+        CHAR_POINT,
+        CHAR_SIGN,
+        CHAR_SPACE,
+        CHAR_ILLEGAL,
+    }
 }
