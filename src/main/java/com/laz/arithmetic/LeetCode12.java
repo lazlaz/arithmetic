63327d6f46384d9a45706105becaf054430b1932
laz <laz>
2020-09-03 21:50:51 +0800

    链表随机节点

--- src/main/java/com/laz/arithmetic/LeetCode12.java
+++ src/main/java/com/laz/arithmetic/LeetCode12.java
@@ -10,6 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -208,7 +209,7 @@ public class LeetCode12 {
 			list = twitter.getNewsFeed(1);
 			System.out.println(Joiner.on(",").join(list));
 		}
-		
+
 		{
 			System.out.println("-------------------");
 			Twitter twitter = new Twitter();
@@ -226,167 +227,168 @@ public class LeetCode12 {
 			List<Integer> list = twitter.getNewsFeed(1);
 			System.out.println(Joiner.on(",").join(list));
 		}
-	
+
 	}
-	
-	//https://leetcode-cn.com/problems/design-twitter/solution/ha-xi-biao-lian-biao-you-xian-dui-lie-java-by-liwe/
+
+	// https://leetcode-cn.com/problems/design-twitter/solution/ha-xi-biao-lian-biao-you-xian-dui-lie-java-by-liwe/
 	public static class Twitter {
 
-	    /**
-	     * 用户 id 和推文（单链表）的对应关系
-	     */
-	    private Map<Integer, Tweet> twitter;
+		/**
+		 * 用户 id 和推文（单链表）的对应关系
+		 */
+		private Map<Integer, Tweet> twitter;
 
-	    /**
-	     * 用户 id 和他关注的用户列表的对应关系
-	     */
-	    private Map<Integer, Set<Integer>> followings;
+		/**
+		 * 用户 id 和他关注的用户列表的对应关系
+		 */
+		private Map<Integer, Set<Integer>> followings;
 
-	    /**
-	     * 全局使用的时间戳字段，用户每发布一条推文之前 + 1
-	     */
-	    private static int timestamp = 0;
+		/**
+		 * 全局使用的时间戳字段，用户每发布一条推文之前 + 1
+		 */
+		private static int timestamp = 0;
 
-	    /**
-	     * 合并 k 组推文使用的数据结构（可以在方法里创建使用），声明成全局变量非必需，视个人情况使用
-	     */
-	    private static PriorityQueue<Tweet> maxHeap;
+		/**
+		 * 合并 k 组推文使用的数据结构（可以在方法里创建使用），声明成全局变量非必需，视个人情况使用
+		 */
+		private static PriorityQueue<Tweet> maxHeap;
 
-	    /**
-	     * Initialize your data structure here.
-	     */
-	    public Twitter() {
-	        followings = new HashMap<>();
-	        twitter = new HashMap<>();
-	        maxHeap = new PriorityQueue<>((o1, o2) -> -o1.timestamp + o2.timestamp);
-	    }
+		/**
+		 * Initialize your data structure here.
+		 */
+		public Twitter() {
+			followings = new HashMap<>();
+			twitter = new HashMap<>();
+			maxHeap = new PriorityQueue<>((o1, o2) -> -o1.timestamp + o2.timestamp);
+		}
 
-	    /**
-	     * Compose a new tweet.
-	     */
-	    public void postTweet(int userId, int tweetId) {
-	        timestamp++;
-	        if (twitter.containsKey(userId)) {
-	            Tweet oldHead = twitter.get(userId);
-	            Tweet newHead = new Tweet(tweetId, timestamp);
-	            newHead.next = oldHead;
-	            twitter.put(userId, newHead);
-	        } else {
-	            twitter.put(userId, new Tweet(tweetId, timestamp));
-	        }
-	    }
+		/**
+		 * Compose a new tweet.
+		 */
+		public void postTweet(int userId, int tweetId) {
+			timestamp++;
+			if (twitter.containsKey(userId)) {
+				Tweet oldHead = twitter.get(userId);
+				Tweet newHead = new Tweet(tweetId, timestamp);
+				newHead.next = oldHead;
+				twitter.put(userId, newHead);
+			} else {
+				twitter.put(userId, new Tweet(tweetId, timestamp));
+			}
+		}
 
-	    /**
-	     * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
-	     */
-	    public List<Integer> getNewsFeed(int userId) {
-	        // 由于是全局使用的，使用之前需要清空
-	        maxHeap.clear();
+		/**
+		 * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in
+		 * the news feed must be posted by users who the user followed or by the user
+		 * herself. Tweets must be ordered from most recent to least recent.
+		 */
+		public List<Integer> getNewsFeed(int userId) {
+			// 由于是全局使用的，使用之前需要清空
+			maxHeap.clear();
 
-	        // 如果自己发了推文也要算上
-	        if (twitter.containsKey(userId)) {
-	            maxHeap.offer(twitter.get(userId));
-	        }
+			// 如果自己发了推文也要算上
+			if (twitter.containsKey(userId)) {
+				maxHeap.offer(twitter.get(userId));
+			}
 
-	        Set<Integer> followingList = followings.get(userId);
-	        if (followingList != null && followingList.size() > 0) {
-	            for (Integer followingId : followingList) {
-	                Tweet tweet = twitter.get(followingId);
-	                if (tweet != null) {
-	                    maxHeap.offer(tweet);
-	                }
-	            }
-	        }
+			Set<Integer> followingList = followings.get(userId);
+			if (followingList != null && followingList.size() > 0) {
+				for (Integer followingId : followingList) {
+					Tweet tweet = twitter.get(followingId);
+					if (tweet != null) {
+						maxHeap.offer(tweet);
+					}
+				}
+			}
 
-	        List<Integer> res = new ArrayList<>(10);
-	        int count = 0;
-	        while (!maxHeap.isEmpty() && count < 10) {
-	            Tweet head = maxHeap.poll();
-	            res.add(head.id);
+			List<Integer> res = new ArrayList<>(10);
+			int count = 0;
+			while (!maxHeap.isEmpty() && count < 10) {
+				Tweet head = maxHeap.poll();
+				res.add(head.id);
 
-	            // 这里最好的操作应该是 replace，但是 Java 没有提供
-	            if (head.next != null) {
-	                maxHeap.offer(head.next);
-	            }
-	            count++;
-	        }
-	        return res;
-	    }
+				// 这里最好的操作应该是 replace，但是 Java 没有提供
+				if (head.next != null) {
+					maxHeap.offer(head.next);
+				}
+				count++;
+			}
+			return res;
+		}
 
+		/**
+		 * Follower follows a followee. If the operation is invalid, it should be a
+		 * no-op.
+		 *
+		 * @param followerId 发起关注者 id
+		 * @param followeeId 被关注者 id
+		 */
+		public void follow(int followerId, int followeeId) {
+			// 被关注人不能是自己
+			if (followeeId == followerId) {
+				return;
+			}
 
-	    /**
-	     * Follower follows a followee. If the operation is invalid, it should be a no-op.
-	     *
-	     * @param followerId 发起关注者 id
-	     * @param followeeId 被关注者 id
-	     */
-	    public void follow(int followerId, int followeeId) {
-	        // 被关注人不能是自己
-	        if (followeeId == followerId) {
-	            return;
-	        }
+			// 获取我自己的关注列表
+			Set<Integer> followingList = followings.get(followerId);
+			if (followingList == null) {
+				Set<Integer> init = new HashSet<>();
+				init.add(followeeId);
+				followings.put(followerId, init);
+			} else {
+				if (followingList.contains(followeeId)) {
+					return;
+				}
+				followingList.add(followeeId);
+			}
+		}
 
-	        // 获取我自己的关注列表
-	        Set<Integer> followingList = followings.get(followerId);
-	        if (followingList == null) {
-	            Set<Integer> init = new HashSet<>();
-	            init.add(followeeId);
-	            followings.put(followerId, init);
-	        } else {
-	            if (followingList.contains(followeeId)) {
-	                return;
-	            }
-	            followingList.add(followeeId);
-	        }
-	    }
+		/**
+		 * Follower unfollows a followee. If the operation is invalid, it should be a
+		 * no-op.
+		 *
+		 * @param followerId 发起取消关注的人的 id
+		 * @param followeeId 被取消关注的人的 id
+		 */
+		public void unfollow(int followerId, int followeeId) {
+			if (followeeId == followerId) {
+				return;
+			}
 
+			// 获取我自己的关注列表
+			Set<Integer> followingList = followings.get(followerId);
 
-	    /**
-	     * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
-	     *
-	     * @param followerId 发起取消关注的人的 id
-	     * @param followeeId 被取消关注的人的 id
-	     */
-	    public void unfollow(int followerId, int followeeId) {
-	        if (followeeId == followerId) {
-	            return;
-	        }
+			if (followingList == null) {
+				return;
+			}
+			// 这里删除之前无需做判断，因为查找是否存在以后，就可以删除，反正删除之前都要查找
+			followingList.remove(followeeId);
+		}
 
-	        // 获取我自己的关注列表
-	        Set<Integer> followingList = followings.get(followerId);
+		/**
+		 * 推文类，是一个单链表（结点视角）
+		 */
+		private class Tweet {
+			/**
+			 * 推文 id
+			 */
+			private int id;
 
-	        if (followingList == null) {
-	            return;
-	        }
-	        // 这里删除之前无需做判断，因为查找是否存在以后，就可以删除，反正删除之前都要查找
-	        followingList.remove(followeeId);
-	    }
+			/**
+			 * 发推文的时间戳
+			 */
+			private int timestamp;
+			private Tweet next;
 
-	    /**
-	     * 推文类，是一个单链表（结点视角）
-	     */
-	    private class Tweet {
-	        /**
-	         * 推文 id
-	         */
-	        private int id;
+			public Tweet(int id, int timestamp) {
+				this.id = id;
+				this.timestamp = timestamp;
+			}
+		}
 
-	        /**
-	         * 发推文的时间戳
-	         */
-	        private int timestamp;
-	        private Tweet next;
-
-	        public Tweet(int id, int timestamp) {
-	            this.id = id;
-	            this.timestamp = timestamp;
-	        }
-	    }
-
-	  
 	}
 
-	//自己设计的
+	// 自己设计的
 	class TwitterMy {
 		// 推特信息列表
 		private Map<Integer, List<Map<Integer, Long>>> tweets = new HashMap<Integer, List<Map<Integer, Long>>>();
@@ -432,7 +434,7 @@ public class LeetCode12 {
 			List<Map<Integer, Long>> tweetList = new ArrayList<Map<Integer, Long>>();
 			tweetList.addAll(tweets.getOrDefault(userId, new LinkedList<Map<Integer, Long>>()));
 			for (Integer followerId : followList) {
-				if (followerId!=userId) {
+				if (followerId != userId) {
 					tweetList.addAll(tweets.getOrDefault(followerId, new LinkedList<Map<Integer, Long>>()));
 				}
 			}
@@ -441,7 +443,7 @@ public class LeetCode12 {
 			for (int i = 0; i < tweetList.size(); i++) {
 				Integer tweetId = (Integer) tweetList.get(i).keySet().toArray()[0];
 				list.add(tweetId);
-				if (list.size()==10) {
+				if (list.size() == 10) {
 					break;
 				}
 			}
@@ -469,4 +471,38 @@ public class LeetCode12 {
 			}
 		}
 	}
+
+	// 链表随机节点
+	@Test
+	public void test4() {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		Solution4 solution = new Solution4(head);
+
+		// getRandom()方法应随机返回1,2,3中的一个，保证每个元素被返回的概率相等。
+		System.out.println(solution.getRandom());
+	}
+
+	class Solution4 {
+		List<Integer> values = new ArrayList<Integer>();
+
+		/**
+		 * @param head The linked list's head. Note that the head is guaranteed to be
+		 *             not null, so it contains at least one node.
+		 */
+		public Solution4(ListNode head) {
+			while (head != null) {
+				values.add(head.val);
+				head = head.next;
+			}
+		}
+
+		/** Returns a random node's value. */
+		public int getRandom() {
+			int index = new Random().nextInt(values.size());
+			return values.get(index);
+		}
+	}
+
 }
