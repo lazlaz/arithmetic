09066682112ce53ee663e9618a9a6033b8f4e9ee
laz <laz>
2020-09-22 19:26:16 +0800

    监控二叉树

--- src/main/java/com/laz/arithmetic/LeetCode13.java
+++ src/main/java/com/laz/arithmetic/LeetCode13.java
@@ -82,4 +82,27 @@ public class LeetCode13 {
     	}
     	return ret;
     }
+    
+    // 监控二叉树
+    @Test
+    public void test() {
+    	Assert.assertEquals(2, minCameraCover(Utils.createTree(new Integer[] {0,0,null,0,null,0,null,null,0})));
+    }
+    public int minCameraCover(TreeNode root) {
+        int[] array = dfs(root);
+        return array[1];
+    }
+
+    public int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{Integer.MAX_VALUE / 2, 0, 0};
+        }
+        int[] leftArray = dfs(root.left);
+        int[] rightArray = dfs(root.right);
+        int[] array = new int[3];
+        array[0] = leftArray[2] + rightArray[2] + 1;
+        array[1] = Math.min(array[0], Math.min(leftArray[0] + rightArray[1], rightArray[0] + leftArray[1]));
+        array[2] = Math.min(array[0], leftArray[1] + rightArray[1]);
+        return array;
+    }
 }
