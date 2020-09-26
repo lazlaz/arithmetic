d0da913c8059ea4c8a6db11df60cf763abb63057
laz <laz>
2020-09-26 21:48:28 +0800

    考场就座

--- src/main/java/com/laz/arithmetic/LeetCode13.java
+++ src/main/java/com/laz/arithmetic/LeetCode13.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -617,4 +618,64 @@ public class LeetCode13 {
 			arrayList.removeLast();
 		}
 	}
+
+	// 考场就座
+	@Test
+	public void test13() {
+		ExamRoom obj = new ExamRoom(10);
+		obj.seat();
+		obj.seat();
+		obj.seat();
+		obj.seat();
+		obj.leave(4);
+		obj.seat();
+	}
+
+	class ExamRoom {
+		int N;
+		TreeSet<Integer> students;
+		public ExamRoom(int N) {
+			this.N = N;
+			students = new TreeSet();
+		}
+		public int seat() {
+			// Let's determine student, the position of the next
+			// student to sit down.
+			int student = 0;
+			if (students.size() > 0) {
+				// Tenatively, dist is the distance to the closest student,
+				// which is achieved by sitting in the position 'student'.
+				// We start by considering the left-most seat.
+				int dist = students.first();
+				Integer prev = null;
+				for (Integer s : students) {
+					if (prev != null) {
+						// For each pair of adjacent students in positions (prev, s),
+						// d is the distance to the closest student;
+						// achieved at position prev + d.
+						int d = (s - prev) / 2;
+						if (d > dist) {
+							dist = d;
+							student = prev + d;
+						}
+					}
+					prev = s;
+				}
+
+				// Considering the right-most seat.
+				if (N - 1 - students.last() > dist)
+					student = N - 1;
+			}
+
+			// Add the student to our sorted TreeSet of positions.
+			students.add(student);
+			return student;
+		}
+
+		public void leave(int p) {
+			students.remove(p);
+		}
+
+	}
+
 }
