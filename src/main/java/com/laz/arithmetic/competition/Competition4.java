9478a5bc0e5030c7e2d2ff8572111feef458751c
laz <laz>
2020-10-11 16:57:19 +0800

    分割两个字符串得到回文串

--- src/main/java/com/laz/arithmetic/competition/Competition4.java
+++ src/main/java/com/laz/arithmetic/competition/Competition4.java
@@ -17,18 +17,18 @@ public class Competition4 {
 	public void test1() {
 		Assert.assertEquals(3, maxDepth("(1+(2*3)+((8)/4))+1"));
 	}
-	
+
 	public int maxDepth(String s) {
-        int max = 0, count = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                max = Math.max(max, ++count);
-            } else if (c == ')') {
-                count--;
-            }
-        }
-        return max;
-    }
+		int max = 0, count = 0;
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				max = Math.max(max, ++count);
+			} else if (c == ')') {
+				count--;
+			}
+		}
+		return max;
+	}
 
 	// 最大网络秩
 	@Test
@@ -43,11 +43,11 @@ public class Competition4 {
 	public int maximalNetworkRank(int n, int[][] roads) {
 		// 计算每个点连接的道路
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		Map<Integer,List<Integer>> edge = new HashMap<Integer,List<Integer>>();
+		Map<Integer, List<Integer>> edge = new HashMap<Integer, List<Integer>>();
 		for (int j = 0; j < roads.length; j++) {
 			List<Integer> r = edge.getOrDefault(roads[j][0], new ArrayList<Integer>());
 			r.add(roads[j][1]);
-			edge.put(roads[j][0],r);
+			edge.put(roads[j][0], r);
 		}
 		for (int i = 0; i < n; i++) {
 			int count = 0;
@@ -55,7 +55,7 @@ public class Competition4 {
 				if (roads[j][0] == i || roads[j][1] == i) {
 					count++;
 				}
-				
+
 			}
 			map.put(i, count);
 		}
@@ -70,28 +70,71 @@ public class Competition4 {
 		int max = 0;
 		int v = list.get(1).getValue();
 		// 注意这里遍历的是list，也就是我们将map.Entry放进了list，排序后的集合
-		for (int i=0;i<list.size();i++) {
-			for (int j=0;j<list.size();j++) {
-				if (i==j) {
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if (i == j) {
 					continue;
 				}
 				Map.Entry<Integer, Integer> s1 = list.get(i);
 				Map.Entry<Integer, Integer> s2 = list.get(j);
-				if (s2.getValue()<v || s1.getValue()<v) {
+				if (s2.getValue() < v || s1.getValue() < v) {
 					break;
 				}
-				int value = s1.getValue()+s2.getValue();
-				if ((edge.get(s1.getKey())!=null && edge.get(s1.getKey()).contains(s2.getKey()))
-						|| (edge.get(s2.getKey())!=null && edge.get(s2.getKey()).contains(s1.getKey()))) {
-					value=value-1;
+				int value = s1.getValue() + s2.getValue();
+				if ((edge.get(s1.getKey()) != null && edge.get(s1.getKey()).contains(s2.getKey()))
+						|| (edge.get(s2.getKey()) != null && edge.get(s2.getKey()).contains(s1.getKey()))) {
+					value = value - 1;
 				}
-				if (max<value) {
+				if (max < value) {
 					max = value;
 				}
 			}
-			
+
 		}
 
 		return max;
 	}
+
+	//分割两个字符串得到回文串
+	@Test
+	public void test3() {
+		Assert.assertEquals(true, new Solution3().checkPalindromeFormation("ulacfd","jizalu"));
+	}
+
+	class Solution3 {
+		public boolean checkPalindromeFormation(String a, String b) {
+			int len = a.length();
+			if (isPalindrome(a, 0, len) || isPalindrome(b, 0, len))
+				return true;
+
+			// prefixA + suffixB
+			int index = 0;
+			while (a.charAt(index) == b.charAt(len - 1 - index))
+				index++;
+			if (isPalindrome(a, index, len - index) || isPalindrome(b, index, len - index))
+				return true;
+			// prefixB + suffixA
+			index = 0;
+			while (a.charAt(len - 1 - index) == b.charAt(index))
+				index++;
+			if (isPalindrome(b, index, len - index) || isPalindrome(a, index, len - index))
+				return true;
+
+			return false;
+		}
+
+		private boolean isPalindrome(String str, int low, int high) {
+			// high is not contained
+			int time = ((high - low) >> 1);
+			for (int i = 0; i < time; i++) {
+				if (str.charAt(low + i) != str.charAt(high - 1 - i))
+					return false;
+			}
+
+			return true;
+		}
+
+	}
+	
+	//
 }
