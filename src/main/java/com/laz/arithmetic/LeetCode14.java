cff9c350c2cf5a91718bce82131798be471f34c5
laz <laz>
2020-10-14 21:06:30 +0800

    排序链表

--- src/main/java/com/laz/arithmetic/LeetCode14.java
+++ src/main/java/com/laz/arithmetic/LeetCode14.java
@@ -629,7 +629,7 @@ public class LeetCode14 {
 	// 最长连续序列
 	@Test
 	public void test16() {
-		Assert.assertEquals(4, longestConsecutive(new int[] { 100, 4, 200, 1, 3, 4,2 }));
+		Assert.assertEquals(4, longestConsecutive(new int[] { 100, 4, 200, 1, 3, 4, 2 }));
 	}
 
 	// https://leetcode-cn.com/problems/longest-consecutive-sequence/solution/zui-chang-lian-xu-xu-lie-by-leetcode-solution/
@@ -657,5 +657,113 @@ public class LeetCode14 {
 
 		return longestStreak;
 
+	}
+
+	// 排序链表
+	@Test
+	public void test17() {
+		ListNode head = Utils.createListNode(new Integer[] { 4, 2, 1, 3 });
+		head = new Solution17().sortList(head);
+		Utils.printListNode(head);
+	}
+
+	// 迭代归并 https://leetcode-cn.com/problems/sort-list/solution/pai-xu-lian-biao-di-gui-die-dai-xiang-jie-by-cherr/
+	class Solution17 {
+		public ListNode sortList(ListNode head) {
+			int length = getLength(head);
+			ListNode dummy = new ListNode(-1);
+			dummy.next = head;
+			
+			for (int step = 1; step < length; step *= 2) { // 依次将链表分成1块，2块，4块...
+				// 每次变换步长，pre指针和cur指针都初始化在链表头
+				ListNode pre = dummy;
+				ListNode cur = dummy.next;
+				while (cur != null) {
+					ListNode h1 = cur; // 第一部分头 （第二次循环之后，cur为剩余部分头，不断往后把链表按照步长step分成一块一块...）
+					ListNode h2 = split(h1, step); // 第二部分头
+					cur = split(h2, step); // 剩余部分的头
+					ListNode temp = merge(h1, h2); // 将一二部分排序合并
+					pre.next = temp; // 将前面的部分与排序好的部分连接
+					while (pre.next != null) {
+						pre = pre.next; // 把pre指针移动到排序好的部分的末尾
+					}
+				}
+			}
+			return dummy.next;
+		}
+		
+		public int getLength(ListNode head) {
+			// 获取链表长度
+			int count = 0;
+			while (head != null) {
+				count++;
+				head = head.next;
+			}
+			return count;
+		}
+		
+		public ListNode split(ListNode head, int step) {
+			// 断链操作 返回第二部分链表头
+			if (head == null)
+				return null;
+			ListNode cur = head;
+			for (int i = 1; i < step && cur.next != null; i++) {
+				cur = cur.next;
+			}
+			ListNode right = cur.next;
+			cur.next = null; // 切断连接
+			return right;
+		}
+		
+		public ListNode merge(ListNode h1, ListNode h2) {
+			// 合并两个有序链表
+			ListNode head = new ListNode(-1);
+			ListNode p = head;
+			while (h1 != null && h2 != null) {
+				if (h1.val < h2.val) {
+					p.next = h1;
+					h1 = h1.next;
+				} else {
+					p.next = h2;
+					h2 = h2.next;
+				}
+				p = p.next;
+			}
+			if (h1 != null)
+				p.next = h1;
+			if (h2 != null)
+				p.next = h2;
+			
+			return head.next;
+		}
+	}
+
+	// 递归归并
+	public ListNode sortList(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		ListNode fast = head.next, slow = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		ListNode tmp = slow.next;
+		slow.next = null;
+		ListNode left = sortList(head);
+		ListNode right = sortList(tmp);
+		ListNode h = new ListNode(0);
+		ListNode res = h;
+		while (left != null && right != null) {
+			if (left.val < right.val) {
+				h.next = left;
+				left = left.next;
+			} else {
+				h.next = right;
+				right = right.next;
+			}
+			h = h.next;
+		}
+		h.next = (left != null ? left : right);
+		return res.next;
 	}
 }
