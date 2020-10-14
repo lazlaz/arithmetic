64eadc58d65c74f3cd4e75586d11eac5b2ba0eca
liuzhai <578091472@qq.com>
2020-10-14 16:42:46 +0800

    从前序与中序遍历序列构造二叉树

--- src/main/java/com/laz/arithmetic/LeetCode14.java
+++ src/main/java/com/laz/arithmetic/LeetCode14.java
@@ -572,4 +572,54 @@ public class LeetCode14 {
 		}
 		return res;
     }
+	
+	//从前序与中序遍历序列构造二叉树
+	@Test
+	public void test15() {
+		TreeNode root = new Solution15().buildTree(new int[] {3,9,20,15,7}, new int[] {9,3,15,20,7});
+		Utils.printTreeNode(root);
+	}
+	
+	class Solution15 {
+		int post_idx;
+		int[] preorder;
+		int[] inorder;
+		Map<Integer, Integer> idx_map = new HashMap<Integer, Integer>();
+
+		public TreeNode helper(int in_left, int in_right) {
+			// 如果这里没有节点构造二叉树了，就结束
+			if (in_left > in_right || post_idx>=preorder.length) {
+				return null;
+			}
+
+			// 选择 post_idx 位置的元素作为当前子树根节点
+			int root_val = preorder[post_idx];
+			TreeNode root = new TreeNode(root_val);
+
+			// 根据 root 所在位置分成左右两棵子树
+			int index = idx_map.get(root_val);
+
+			post_idx++;
+			// 构造左子树
+			root.left = helper(in_left, index - 1);
+			// 构造右子树
+			root.right = helper(index + 1, in_right);
+			return root;
+		}
+
+		public TreeNode buildTree(int[] preorder, int[] inorder) {
+			this.preorder = preorder;
+			this.inorder = inorder;
+			// 从前序遍历的第一个元素开始
+			post_idx = 0;
+
+			// 建立（元素，下标）键值对的哈希表
+			int idx = 0;
+			for (Integer val : inorder) {
+				idx_map.put(val, idx++);
+			}
+
+			return helper(0, inorder.length - 1);
+		}
+	}
 }
