c9f431cd70a7daa09df87048655311e90c19bcec
laz <laz>
2020-11-01 16:03:47 +0800

    单词拆分 II

--- src/main/java/com/laz/arithmetic/LeetCode1.java
+++ src/main/java/com/laz/arithmetic/LeetCode1.java
@@ -1,8 +1,9 @@
 package com.laz.arithmetic;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -11,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.TreeMap;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -100,33 +100,71 @@ public class LeetCode1 {
 		wordDict.add("applepen");
 		wordDict.add("pine");
 		wordDict.add("pineapple");
-		System.out.println(wordBreak2("pineapplepenapple", wordDict));
+		System.out.println(new Solution4().wordBreak("pineapplepenapple", wordDict));
 	}
 
-	public List<String> wordBreak2(String s, List<String> wordDict) {
-		LinkedList<String>[] dp = new LinkedList[s.length() + 1];
-		LinkedList<String> initial = new LinkedList<>();
-		initial.add("");
-		dp[0] = initial;
-		int maxLength = 0;
-		for (String str : wordDict) {
-			if (maxLength < str.length()) {
-				maxLength = str.length();
-			}
-		}
-		for (int i = 1; i <= s.length(); i++) {
-			LinkedList<String> list = new LinkedList<>();
-			for (int j = 0; j < i; j++) {
-				if (dp[j].size() > 0 && wordDict.contains(s.substring(j, i))) {
-					for (String l : dp[j]) {
-						list.add(l + (l.equals("") ? "" : " ") + s.substring(j, i));
-					}
-				}
-			}
-			dp[i] = list;
-		}
-		return dp[s.length()];
+	//https://leetcode-cn.com/problems/word-break-ii/solution/dong-tai-gui-hua-hui-su-qiu-jie-ju-ti-zhi-python-d/
+	class Solution4 {
+		  public List<String> wordBreak(String s, List<String> wordDict) {
+		        // 为了快速判断一个单词是否在单词集合中，需要将它们加入哈希表
+		        Set<String> wordSet = new HashSet<>(wordDict);
+		        int len = s.length();
 
+		        // 第 1 步：动态规划计算是否有解
+		        // dp[i] 表示「长度」为 i 的 s 前缀子串可以拆分成 wordDict 中的单词
+		        // 长度包括 0 ，因此状态数组的长度为 len + 1
+		        boolean[] dp = new boolean[len + 1];
+		        // 0 这个值需要被后面的状态值参考，如果一个单词正好在 wordDict 中，dp[0] 设置成 true 是合理的
+		        dp[0] = true;
+
+		        for (int right = 1; right <= len; right++) {
+		            // 如果单词集合中的单词长度都不长，从后向前遍历是更快的
+		            for (int left = right - 1; left >= 0; left--) {
+		                // substring 不截取 s[right]，dp[left] 的结果不包含 s[left]
+		                if (wordSet.contains(s.substring(left, right)) && dp[left]) {
+		                    dp[right] = true;
+		                    // 这个 break 很重要，一旦得到 dp[right] = True ，不必再计算下去
+		                    break;
+		                }
+		            }
+		        }
+
+		        // 第 2 步：回溯算法搜索所有符合条件的解
+		        List<String> res = new ArrayList<>();
+		        if (dp[len]) {
+		            Deque<String> path = new ArrayDeque<>();
+		            dfs(s, len, wordSet, dp, path, res);
+		            return res;
+		        }
+		        return res;
+		    }
+
+		    /**
+		     * s[0:len) 如果可以拆分成 wordSet 中的单词，把递归求解的结果加入 res 中
+		     *
+		     * @param s
+		     * @param len     长度为 len 的 s 的前缀子串
+		     * @param wordSet 单词集合，已经加入哈希表
+		     * @param dp      预处理得到的 dp 数组
+		     * @param path    从叶子结点到根结点的路径
+		     * @param res     保存所有结果的变量
+		     */
+		    private void dfs(String s, int len, Set<String> wordSet, boolean[] dp, Deque<String> path, List<String> res) {
+		        if (len == 0) {
+		            res.add(String.join(" ",path));
+		            return;
+		        }
+
+		        // 可以拆分的左边界从 len - 1 依次枚举到 0
+		        for (int i = len - 1; i >= 0; i--) {
+		            String suffix = s.substring(i, len);
+		            if (wordSet.contains(suffix) && dp[i]) {
+		                path.addFirst(suffix);
+		                dfs(s, i, wordSet, dp, path, res);
+		                path.removeFirst();
+		            }
+		        }
+		    }
 	}
 
 	// 回文数
@@ -467,7 +505,8 @@ public class LeetCode1 {
 		node11.right = node211;
 		System.out.println(isSameTree(node, node11));
 	}
-	//递归同时判断解法
+
+	// 递归同时判断解法
 	public boolean isSameTree2(TreeNode p, TreeNode q) {
 		if (p == null && q == null) {
 			return true;
@@ -536,33 +575,33 @@ public class LeetCode1 {
 		}
 
 	}
-	
-    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
-    	List<List<Integer>> ret = new ArrayList<List<Integer>>();
-    	if (root == null) {
-    		return ret;
-    	}
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-        	List<Integer> level = new ArrayList<Integer>();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-                TreeNode left = node.left, right = node.right;
-                if (left != null) {
-                    queue.offer(left);
-                }
-                if (right != null) {
-                    queue.offer(right);
-                }
-            }
-            ret.add(0, level);
-        }
-        return ret;
-    }
-    
+
+	public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+		List<List<Integer>> ret = new ArrayList<List<Integer>>();
+		if (root == null) {
+			return ret;
+		}
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			List<Integer> level = new ArrayList<Integer>();
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				level.add(node.val);
+				TreeNode left = node.left, right = node.right;
+				if (left != null) {
+					queue.offer(left);
+				}
+				if (right != null) {
+					queue.offer(right);
+				}
+			}
+			ret.add(0, level);
+		}
+		return ret;
+	}
+
 	public List<List<Integer>> levelOrderBottom(TreeNode root) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		Map<Integer, List<Integer>> map = new LinkedHashMap<Integer, List<Integer>>();
