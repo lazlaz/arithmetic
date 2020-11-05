904dfed94a305b6f3d1a31220f6f94c22607a1f7
liuzhai <578091472@qq.com>
2020-11-05 16:06:36 +0800

    直线上最多的点数

--- src/main/java/com/laz/arithmetic/LeetCode16.java
+++ src/main/java/com/laz/arithmetic/LeetCode16.java
@@ -541,4 +541,58 @@ public class LeetCode16 {
 			return n;
 		}
 	}
+	
+	//直线上最多的点数
+	@Test
+	public void test10() {
+		Assert.assertEquals(3, new Solution10().maxPoints(new int[][] {
+			{1,1},{2,2},{3,3}
+		}));
+	}
+	//https://leetcode-cn.com/problems/max-points-on-a-line/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--35/
+	class Solution10 {
+		public int maxPoints(int[][] points) {
+		    if (points.length < 3) {
+		        return points.length;
+		    }
+		    int res = 0;
+		    //遍历每个点
+		    for (int i = 0; i < points.length; i++) {
+		        int duplicate = 0;
+		        int max = 0;//保存经过当前点的直线中，最多的点
+		        HashMap<String, Integer> map = new HashMap<>();
+		        for (int j = i + 1; j < points.length; j++) {
+		            //求出分子分母
+		            int x = points[j][0] - points[i][0];
+		            int y = points[j][1] - points[i][1];
+		            if (x == 0 && y == 0) {
+		                duplicate++;
+		                continue;
+
+		            }
+		            //进行约分
+		            int gcd = gcd(x, y);
+		            x = x / gcd;
+		            y = y / gcd;
+		            String key = x + "@" + y;
+		            map.put(key, map.getOrDefault(key, 0) + 1);
+		            max = Math.max(max, map.get(key));
+		        }
+		        //1 代表当前考虑的点，duplicate 代表和当前的点重复的点
+		        res = Math.max(res, max + duplicate + 1);
+		    }
+		    return res;
+		}
+
+		private int gcd(int a, int b) {
+		    while (b != 0) {
+		        int temp = a % b;
+		        a = b;
+		        b = temp;
+		    }
+		    return a;
+		}
+
+	}
+	   
 }
