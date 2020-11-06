ef375e10b64bc336676306136aded3f580c2d2d7
liuzhai <578091472@qq.com>
2020-11-06 14:51:37 +0800

    根据数字二进制下 1 的数目排序

--- src/main/java/com/laz/arithmetic/LeetCode16.java
+++ src/main/java/com/laz/arithmetic/LeetCode16.java
@@ -673,6 +673,7 @@ public class LeetCode16 {
 				new Solution13().sortByBits(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }));
 	}
 	class Solution13 {
+		
 		public int[] sortByBits(int[] arr) {
 			List<Integer> list = new ArrayList<Integer>();
 			for (int i=0;i<arr.length;i++) {
