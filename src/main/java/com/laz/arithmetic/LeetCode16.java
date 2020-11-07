465064db2807fd0565fbb83e739d7bd09a441ec8
laz <laz>
2020-11-07 12:01:49 +0800

    区间和的个数

--- src/main/java/com/laz/arithmetic/LeetCode16.java
+++ src/main/java/com/laz/arithmetic/LeetCode16.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -560,5 +561,94 @@ public class LeetCode16 {
 		}
 		return l;
 
+	}
+	
+	//区间和的个数
+	@Test
+	public void test14() {
+		Assert.assertEquals(3, new Solution14().countRangeSum(new int[] {
+				-2,5,-1
+		}, -2, 2));
+	}
+	//https://leetcode-cn.com/problems/count-of-range-sum/solution/qu-jian-he-de-ge-shu-by-leetcode-solution/
+	class Solution14{
+		public int countRangeSum(int[] nums, int lower, int upper) {
+	        long sum = 0;
+	        long[] preSum = new long[nums.length + 1];
+	        for (int i = 0; i < nums.length; ++i) {
+	            sum += nums[i];
+	            preSum[i + 1] = sum;
+	        }
+	        
+	        Set<Long> allNumbers = new TreeSet<Long>();
+	        for (long x : preSum) {
+	            allNumbers.add(x);
+	            allNumbers.add(x - lower);
+	            allNumbers.add(x - upper);
+	        }
+	        // 利用哈希表进行离散化
+	        Map<Long, Integer> values = new HashMap<Long, Integer>();
+	        int idx = 0;
+	        for (long x : allNumbers) {
+	            values.put(x, idx);
+	            idx++;
+	        }
+
+	        SegNode root = build(0, values.size() - 1);
+	        int ret = 0;
+	        for (long x : preSum) {
+	            int left = values.get(x - upper), right = values.get(x - lower);
+	            ret += count(root, left, right);
+	            insert(root, values.get(x));
+	        }
+	        return ret;
+	    }
+
+	    public SegNode build(int left, int right) {
+	        SegNode node = new SegNode(left, right);
+	        if (left == right) {
+	            return node;
+	        }
+	        int mid = (left + right) / 2;
+	        node.lchild = build(left, mid);
+	        node.rchild = build(mid + 1, right);
+	        return node;
+	    }
+
+	    public int count(SegNode root, int left, int right) {
+	        if (left > root.hi || right < root.lo) {
+	            return 0;
+	        }
+	        if (left <= root.lo && root.hi <= right) {
+	            return root.add;
+	        }
+	        return count(root.lchild, left, right) + count(root.rchild, left, right);
+	    }
+
+	    public void insert(SegNode root, int val) {
+	        root.add++;
+	        if (root.lo == root.hi) {
+	            return;
+	        }
+	        int mid = (root.lo + root.hi) / 2;
+	        if (val <= mid) {
+	            insert(root.lchild, val);
+	        } else {
+	            insert(root.rchild, val);
+	        }
+	    }
+	}
+
+	class SegNode {
+	    int lo, hi, add;
+	    SegNode lchild, rchild;
+
+	    public SegNode(int left, int right) {
+	        lo = left;
+	        hi = right;
+	        add = 0;
+	        lchild = null;
+	        rchild = null;
+	    }
 	}
 }
