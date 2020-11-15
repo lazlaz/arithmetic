00550c8896fa472d89823894e6e3cd18559b4e8c
zhouxinggint@newrnak.cn <laz@DESKTOP-GTUC085>
2020-11-15 21:59:14 +0800

    确定两个字符串是否接近

--- src/main/java/com/laz/arithmetic/competition/Competition9.java
+++ src/main/java/com/laz/arithmetic/competition/Competition9.java
@@ -76,7 +76,7 @@ public class Competition9 {
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
 		for (int i=0;i<arr1.length;i++) {
-			if (arr1[i] !=arr1[i]) {
+			if (arr1[i] !=arr2[i]) {
 				return false;
 			}
 		}
