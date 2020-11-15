d2a179659d0e06f04b04afd01f4c31f5a4ec9240
Admin <Admin@laz>
2020-11-15 10:39:52 +0800

    移掉K位数字

--- src/main/java/com/laz/arithmetic/LeetCode17.java
+++ src/main/java/com/laz/arithmetic/LeetCode17.java
@@ -1,8 +1,10 @@
 package com.laz.arithmetic;
 
-import org.junit.Test;
+import java.util.Deque;
+import java.util.LinkedList;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class LeetCode17 {
 	// 按奇偶排序数组 II
@@ -13,16 +15,52 @@ public class LeetCode17 {
 
 	public int[] sortArrayByParityII(int[] A) {
 		int[] res = new int[A.length];
-		int p=0,q=1;
-		for (int i=0;i<A.length;i++) {
-			if (A[i]%2==0) {
+		int p = 0, q = 1;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] % 2 == 0) {
 				res[p] = A[i];
-				p=p+2;
+				p = p + 2;
 			} else {
 				res[q] = A[i];
-				q=q+2;
+				q = q + 2;
 			}
 		}
 		return res;
+	}
+
+	//移掉K位数字
+	@Test
+	public void test2() {
+		Assert.assertEquals("1219", removeKdigits("1432219", 3));
+	}
+
+	// https://leetcode-cn.com/problems/remove-k-digits/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-5/
+	public String removeKdigits(String num, int k) {
+		Deque<Character> stack = new LinkedList<Character>();
+		for (int i=0;i<num.length();i++) {
+			char c = num.charAt(i);
+			while(!stack.isEmpty() && k>0 && stack.peekLast()>c) {
+				stack.pollLast();
+				k--;
+			}
+			stack.offerLast(c);
+		}
+		//如果K还没有减完，继续移除
+		for (int i = 0; i < k; ++i) {
+			stack.pollLast();
+        }
+        
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!stack.isEmpty()) {
+            char digit = stack.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+
 	}
 }
