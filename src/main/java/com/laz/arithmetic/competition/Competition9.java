56e48e3c2598f74997e19d1e6bec80ac7dcfbf14
zhouxinggint@newrnak.cn <laz@DESKTOP-GTUC085>
2020-11-15 21:58:01 +0800

    确定两个字符串是否接近

--- src/main/java/com/laz/arithmetic/competition/Competition9.java
+++ src/main/java/com/laz/arithmetic/competition/Competition9.java
@@ -1,9 +1,11 @@
 package com.laz.arithmetic.competition;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
@@ -40,4 +42,44 @@ public class Competition9 {
 	    	return res;
 	    }
 	}
+	
+	//确定两个字符串是否接近
+	@Test
+	public void test2() {
+		Assert.assertEquals(true,closeStrings("abc", "bca"));
+		Assert.assertEquals(false,closeStrings("a", "aa"));
+		Assert.assertEquals(true,closeStrings("cabbba", "abbccc"));
+		Assert.assertEquals(false,closeStrings("cabbba", "aabbss"));
+	}
+	public boolean closeStrings(String word1, String word2) {
+		if (word1.length()!=word2.length()) {
+			return false;
+		}
+		int[] arr1 = new int[26];
+		for (int i=0;i<word1.length();i++) {
+			arr1[word1.charAt(i)-'a']++;
+		}
+		int[] arr2 = new int[26];
+		for (int i=0;i<word2.length();i++) {
+			arr2[word2.charAt(i)-'a']++;
+		}
+		//判断是否有存在word1不存在word2的字符，反之也是
+		for (int i=0;i<arr1.length;i++) {
+			if (arr1[i]>0 && arr2[i]==0) {
+				return false;
+			}
+			if (arr2[i]>0 && arr1[i]==0) {
+				return false;
+			}
+		}
+		//排序后判断数组字符数量是否一一匹配，如abbzzca和babzzcz，字符数量分别为2、2、2、1和3、2、1、1则不可能匹配
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		for (int i=0;i<arr1.length;i++) {
+			if (arr1[i] !=arr1[i]) {
+				return false;
+			}
+		}
+		return true;
+    }
 }
