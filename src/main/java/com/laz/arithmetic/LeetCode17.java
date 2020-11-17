4198801de3422ebf976ce2cd25f2b7ecb7b3622c
zhouxinggint@newrnak.cn <laz@DESKTOP-GTUC085>
2020-11-17 21:47:30 +0800

    单词搜索 II

--- src/main/java/com/laz/arithmetic/LeetCode17.java
+++ src/main/java/com/laz/arithmetic/LeetCode17.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -73,27 +75,243 @@ public class LeetCode17 {
 	// 距离顺序排列矩阵单元格
 	@Test
 	public void test3() {
-		Assert.assertArrayEquals(new int[][] {
-			{0,1},{0,0},{1,1},{1,0}
-		}, allCellsDistOrder(2,2,0,1));
+		Assert.assertArrayEquals(new int[][] { { 0, 1 }, { 0, 0 }, { 1, 1 }, { 1, 0 } }, allCellsDistOrder(2, 2, 0, 1));
 	}
 
 	public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
 		List<int[]> list = new ArrayList<int[]>();
-		for (int i=0;i<R;i++) {
-			for (int j=0;j<C;j++) {
-				list.add(new int[] {i,j});
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				list.add(new int[] { i, j });
 			}
 		}
 		Collections.sort(list, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				int distance1 = Math.abs(o1[0]-r0)+Math.abs(o1[1]-c0);
-				int distance2 = Math.abs(o2[0]-r0)+Math.abs(o2[1]-c0);
-				return distance1-distance2;
+				int distance1 = Math.abs(o1[0] - r0) + Math.abs(o1[1] - c0);
+				int distance2 = Math.abs(o2[0] - r0) + Math.abs(o2[1] - c0);
+				return distance1 - distance2;
 			}
 		});
-		int[][] res = new int[R*C][2];
+		int[][] res = new int[R * C][2];
 		return list.toArray(res);
+	}
+
+	// 单词搜索 II
+	@Test
+	public void test4() {
+		{
+			List<String> list = new Solution4().findWords(new char[][] { { 'o', 'a', 'a', 'n' }, { 'e', 't', 'a', 'e' },
+					{ 'i', 'h', 'k', 'r' }, { 'i', 'f', 'l', 'v' } }, new String[] { "oath", "pea", "eat", "rain" });
+			Assert.assertArrayEquals(new String[] { "oath", "eat" }, list.toArray(new String[] {}));
+		}
+		{
+			List<String> list = new Solution4().findWords(new char[][] { { 'a' }, { 'a' } }, new String[] { "aaa" });
+			Assert.assertArrayEquals(new String[] {}, list.toArray(new String[] {}));
+		}
+
+	}
+
+	class Solution4_2 {
+
+		private Set<String> result = new HashSet<>();
+
+		public List<String> findWords(char[][] board, String[] words) {
+			Trie trie = new Trie();
+			for (String word : words) {
+				trie.insert(word);
+			}
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[0].length; j++) {
+					dfs(trie, board, new StringBuilder(), i, j, new int[board.length][board[0].length]);
+				}
+			}
+			return new ArrayList<>(result);
+		}
+
+		private void dfs(Trie parent, char[][] board, StringBuilder prefix, int i, int j, int[][] used) {
+			if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || used[i][j] == 1) {
+				return;
+			}
+			Trie trie = parent.indexOf(board[i][j]);
+			if (trie == null) {
+				return;
+			}
+			used[i][j] = 1;
+			prefix.append(board[i][j]);
+			if (trie.isEnd()) {
+				result.add(prefix.toString());
+			}
+			dfs(trie, board, prefix, i - 1, j, used);
+			dfs(trie, board, prefix, i + 1, j, used);
+			dfs(trie, board, prefix, i, j - 1, used);
+			dfs(trie, board, prefix, i, j + 1, used);
+			used[i][j] = 0;
+			prefix.deleteCharAt(prefix.length() - 1);
+		}
+
+		class Trie {
+			private Trie[] nexts;
+
+			private boolean isEnd;
+
+			public Trie() {
+				nexts = new Trie[26];
+				isEnd = false;
+			}
+
+			public void insert(String word) {
+				Trie temp = this;
+				for (int i = 0; i < word.length(); i++) {
+					temp = temp.put(word.charAt(i));
+				}
+				temp.isEnd(true);
+			}
+
+			public Trie put(Character c) {
+				if (nexts[c - 'a'] == null) {
+					nexts[c - 'a'] = new Trie();
+				}
+				return nexts[c - 'a'];
+			}
+
+			private Trie indexOf(char c) {
+				return nexts[c - 'a'];
+			}
+
+			public Trie indexOf(String prefix) {
+				Trie temp = this;
+				for (int i = 0; i < prefix.length(); i++) {
+					if (temp == null) {
+						return null;
+					}
+					temp = temp.indexOf(prefix.charAt(i));
+				}
+				return temp;
+			}
+
+			public void isEnd(boolean isEnd) {
+				this.isEnd = isEnd;
+			}
+
+			public boolean isEnd() {
+				return this.isEnd;
+			}
+		}
+	}
+
+	// 前缀树+剪枝版本
+	class Solution4 {
+		private int rows, cols;
+		private char[][] board;
+		// Trie的根节点
+		private Node root;
+		// DFS遍历方向
+		private int[][] direct = { { 0, 1, 0, -1 }, { -1, 0, 1, 0 } };
+		private Set<String> ans;
+		// 标记是否已经访问
+		private boolean[][] marked;
+
+		// Trie需要的节点
+		class Node {
+			HashMap<Character, Node> children;
+			String word;
+
+			public Node(String word) {
+				this.children = new HashMap<>();
+				this.word = word;
+			}
+
+			public Node() {
+				this.children = new HashMap<>();
+				this.word = null;
+			}
+		}
+
+		// Trie的操作
+		private void delete(String str) {
+			delete(root, str, 0);
+		}
+
+		private Node delete(Node current, String str, int d) {
+			if (current == null)
+				return null;
+			if (d == str.length()) {
+				if (current.children.isEmpty())
+					return null;
+				current.word = null;
+				return current;
+			}
+			char c = str.charAt(d);
+			Node next = delete(current.children.get(c), str, d + 1);
+			current.children.put(c, next);
+			if (next == null && current.word == null && current.children.size() <= 1)
+				return null;
+			return current;
+		}
+
+		private void insert(String str) {
+			insert(root, str, 0);
+		}
+
+		private Node insert(Node current, String str, int d) {
+			if (current == null)
+				current = new Node();
+			if (d == str.length()) {
+				current.word = str;
+				return current;
+			}
+			char c = str.charAt(d);
+			current.children.put(c, insert(current.children.get(c), str, d + 1));
+			return current;
+		}
+
+		public List<String> findWords(char[][] board, String[] words) {
+			this.rows = board.length;
+			this.cols = board[0].length;
+			this.board = board;
+			this.root = new Node();
+			this.ans = new HashSet<>();
+			this.marked = new boolean[rows][cols];
+			int len = words.length;
+
+			for (int i = 0; i < len; i++) {
+				insert(words[i]);
+			}
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					char c = board[i][j];
+					if (root.children.get(c) != null)
+						dfs(i, j, root.children.get(c));
+				}
+			}
+			return new ArrayList<>(this.ans);
+		}
+
+		private void dfs(int row, int col, Node current) {
+			if (current == null)
+				return;
+			if (current.word != null) {
+				this.ans.add(current.word);
+				delete(current.word);
+			}
+			if (current.children.isEmpty())
+				return;
+			marked[row][col] = true;
+			for (int i = 0; i < 4; i++) {
+				int newRow = row + direct[0][i];
+				int newCol = col + direct[1][i];
+				if (valid(newRow, newCol)) {
+					char c = board[newRow][newCol];
+					if (current.children.get(c) != null)
+						dfs(newRow, newCol, current.children.get(c));
+				}
+			}
+			marked[row][col] = false;
+		}
+
+		private boolean valid(int row, int col) {
+			return row < rows && row >= 0 && col < cols && col >= 0 && !marked[row][col];
+		}
 	}
 }
