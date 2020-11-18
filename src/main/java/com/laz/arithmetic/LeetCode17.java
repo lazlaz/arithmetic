60780400ee0ec29cb62526579cee6d3cf34d64cd
liuzhai <578091472@qq.com>
2020-11-18 18:52:31 +0800

    摆动排序 II

--- src/main/java/com/laz/arithmetic/LeetCode17.java
+++ src/main/java/com/laz/arithmetic/LeetCode17.java
@@ -1,6 +1,7 @@
 package com.laz.arithmetic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
@@ -314,4 +315,93 @@ public class LeetCode17 {
 			return row < rows && row >= 0 && col < cols && col >= 0 && !marked[row][col];
 		}
 	}
+
+	// 摆动排序 II
+	@Test
+	public void test5() {
+//		{
+//			int[] res = new int[] { 1, 5, 1, 1, 6, 4 };
+//			new Solution5().wiggleSort(res);
+//			Assert.assertArrayEquals(new int[] { 1, 6, 1, 5, 1, 4 }, res);
+//		}
+//
+//		{
+//			int[] res = new int[] { 1, 3, 2, 2, 3, 1 };
+//			new Solution5().wiggleSort(res);
+//			Assert.assertArrayEquals(new int[] { 2, 3, 1, 3, 1, 2 }, res);
+//		}
+//		{
+//			int[] res = new int[] { 4, 5, 5, 6 };
+//			new Solution5().wiggleSort(res);
+//			Assert.assertArrayEquals(new int[] { 5, 6, 4, 5 }, res);
+//		}
+		{
+			int[] res = new int[] { 2,1,1,2,1,3,3,3,1,3,1,3,2 };
+			new Solution5().wiggleSort(res);
+			Assert.assertArrayEquals(new int[] { 2,3,2,3,1,3,1,3,1,3,1,2,1 }, res);
+		}
+		
+	}
+
+	// https://leetcode-cn.com/problems/wiggle-sort-ii/solution/yi-bu-yi-bu-jiang-shi-jian-fu-za-du-cong-onlognjia/
+	//https://leetcode-cn.com/problems/wiggle-sort-ii/solution/bai-dong-pai-xu-ii-jie-ti-bao-gao-shi-jian-fu-za-d/
+	class Solution5 {
+		int n=-1;
+	    public void wiggleSort(int[] nums) {
+	        //找到中位数索引
+	        int midIndex = this.quickSelect(nums,0,nums.length-1);
+	        //找到中位数
+	        int mid = nums[midIndex];
+	        n=nums.length;
+	        //三分法
+	        for(int i=0,j=0,k=nums.length-1;j<=k;){
+	            if(nums[V(j)]>mid){
+	                swap(nums,V(j++),V(i++));
+	            }else if(nums[V(j)]<mid){
+	                swap(nums,V(j),V(k--));
+	            }else{
+	                j++;
+	            }
+	        }
+	    }
+	    
+	    public int V(int i){
+	        return (1+2*(i)) % (n|1);
+	    }
+	    
+	    public void swap(int[] nums,int i,int j){
+	        int t = nums[i];
+	        nums[i]=nums[j];
+	        nums[j]=t;
+	    }
+	    
+	    public int quickSelect(int[] nums,int left,int right){
+	        int pivot = nums[left];
+	        int l = left;
+	        int r = right;
+	        while(l<r){
+	            while(l<r&&nums[r]>=pivot){
+	                r--;
+	            }
+	            if(l<r){
+	                nums[l++]=nums[r];
+	            }
+	            while(l<r&&nums[l]<=pivot){
+	                l++;
+	            }
+	            if(l<r){
+	                nums[r--]=nums[l];
+	            }
+	        }
+	        nums[l]=pivot;
+	        if(l==nums.length/2){
+	            return l;
+	        }else if(l>nums.length/2){
+	            return this.quickSelect(nums,left,l-1);
+	        }else{
+	            return this.quickSelect(nums,l+1,right);
+	        }
+	    }
+	}
+
 }
