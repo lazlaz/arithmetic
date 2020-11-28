d303f1d7ed278138e0e49e5b88cdcad2deac93b2
zhouxingting@newrank.cn <zhouxingting@newrank.cn>
2020-11-28 15:38:47 +0800

    翻转对

--- src/main/java/com/laz/arithmetic/LeetCode18.java
+++ src/main/java/com/laz/arithmetic/LeetCode18.java
@@ -1,6 +1,7 @@
 package com.laz.arithmetic;
 
 import java.util.Arrays;
+import java.util.TreeMap;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,5 +27,69 @@ public class LeetCode18 {
 		return max;
 	}
 
+	// 翻转对
+	@Test
+	public void test2() {
+		Assert.assertEquals(2, new Solution2().reversePairs(new int[] { 1, 3, 2, 3, 1 }));
 
+		Assert.assertEquals(3, new Solution2().reversePairs(new int[] { 2, 4, 3, 5, 1 }));
+
+		Assert.assertEquals(1, new Solution2().reversePairs(new int[] { -5, -5 }));
+		Assert.assertEquals(0,
+				new Solution2().reversePairs(new int[] { 2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647 }));
+	}
+
+	// https://leetcode-cn.com/problems/reverse-pairs/solution/fan-zhuan-dui-by-leetcode-solution/
+	class Solution2 {
+		public int reversePairs(int[] nums) {
+			if (nums.length == 0) {
+				return 0;
+			}
+			return reversePairsRecursive(nums, 0, nums.length - 1);
+		}
+
+		public int reversePairsRecursive(int[] nums, int left, int right) {
+			if (left == right) {
+				return 0;
+			} else {
+				int mid = (left + right) / 2;
+				int n1 = reversePairsRecursive(nums, left, mid);
+				int n2 = reversePairsRecursive(nums, mid + 1, right);
+				int ret = n1 + n2;
+
+				// 首先统计下标对的数量
+				int i = left;
+				int j = mid + 1;
+				while (i <= mid) {
+					while (j <= right && (long) nums[i] > 2 * (long) nums[j]) {
+						j++;
+					}
+					ret += j - mid - 1;
+					i++;
+				}
+
+				// 随后合并两个排序数组
+				int[] sorted = new int[right - left + 1];
+				int p1 = left, p2 = mid + 1;
+				int p = 0;
+				while (p1 <= mid || p2 <= right) {
+					if (p1 > mid) {
+						sorted[p++] = nums[p2++];
+					} else if (p2 > right) {
+						sorted[p++] = nums[p1++];
+					} else {
+						if (nums[p1] < nums[p2]) {
+							sorted[p++] = nums[p1++];
+						} else {
+							sorted[p++] = nums[p2++];
+						}
+					}
+				}
+				for (int k = 0; k < sorted.length; k++) {
+					nums[left + k] = sorted[k];
+				}
+				return ret;
+			}
+		}
+	}
 }
