69f9076089e7b4bdb066ccf0a0ea82029c3a60a1
zhouxinggint@newrnak.cn <laz@DESKTOP-GTUC085>
2020-12-02 21:19:43 +0800

    拼接最大数

--- src/main/java/com/laz/arithmetic/LeetCode18.java
+++ src/main/java/com/laz/arithmetic/LeetCode18.java
@@ -117,11 +117,12 @@ public class LeetCode18 {
 	public void test4() {
 		Assert.assertEquals("aba", new Solution4().reorganizeString("aab"));
 		Assert.assertEquals("", new Solution4().reorganizeString("aaab"));
-	    Assert.assertEquals("acabababab", new Solution4().reorganizeString("aaaaabbbbc"));
+		Assert.assertEquals("acabababab", new Solution4().reorganizeString("aaaaabbbbc"));
 		Assert.assertEquals("vovlv", new Solution4().reorganizeString("vvvlo"));
 		Assert.assertEquals("ababababab", new Solution4().reorganizeString("abbabbaaab"));
 		Assert.assertEquals("czcxmwbtcsmlombockgi", new Solution4().reorganizeString("ogccckcwmbmxtsbmozli"));
 	}
+
 	class Solution4 {
 		public String reorganizeString(String S) {
 			int[] arr = new int[26];
@@ -170,5 +171,84 @@ public class LeetCode18 {
 			return sb.toString();
 		}
 
+	}
+
+	// 拼接最大数
+	@Test
+	public void test5() {
+		Assert.assertArrayEquals(new int[] { 9, 8, 6, 5, 3 },
+				new Solution5().maxNumber(new int[] { 3, 4, 6, 5 }, new int[] { 9, 1, 2, 5, 8, 3 }, 5));
+	}
+
+	// https://leetcode-cn.com/problems/create-maximum-number/solution/pin-jie-zui-da-shu-by-leetcode-solution/
+	class Solution5 {
+		public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+			int m = nums1.length, n = nums2.length;
+			int[] maxSubsequence = new int[k];
+			int start = Math.max(0, k - n), end = Math.min(k, m);
+			for (int i = start; i <= end; i++) {
+				int[] subsequence1 = maxSubsequence(nums1, i);
+				int[] subsequence2 = maxSubsequence(nums2, k - i);
+				int[] curMaxSubsequence = merge(subsequence1, subsequence2);
+				if (compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0) {
+					System.arraycopy(curMaxSubsequence, 0, maxSubsequence, 0, k);
+				}
+			}
+			return maxSubsequence;
+		}
+
+		public int[] maxSubsequence(int[] nums, int k) {
+			int length = nums.length;
+			int[] stack = new int[k];
+			int top = -1;
+			int remain = length - k;
+			for (int i = 0; i < length; i++) {
+				int num = nums[i];
+				while (top >= 0 && stack[top] < num && remain > 0) {
+					top--;
+					remain--;
+				}
+				if (top < k - 1) {
+					stack[++top] = num;
+				} else {
+					remain--;
+				}
+			}
+			return stack;
+		}
+
+		public int[] merge(int[] subsequence1, int[] subsequence2) {
+			int x = subsequence1.length, y = subsequence2.length;
+			if (x == 0) {
+				return subsequence2;
+			}
+			if (y == 0) {
+				return subsequence1;
+			}
+			int mergeLength = x + y;
+			int[] merged = new int[mergeLength];
+			int index1 = 0, index2 = 0;
+			for (int i = 0; i < mergeLength; i++) {
+				if (compare(subsequence1, index1, subsequence2, index2) > 0) {
+					merged[i] = subsequence1[index1++];
+				} else {
+					merged[i] = subsequence2[index2++];
+				}
+			}
+			return merged;
+		}
+
+		public int compare(int[] subsequence1, int index1, int[] subsequence2, int index2) {
+			int x = subsequence1.length, y = subsequence2.length;
+			while (index1 < x && index2 < y) {
+				int difference = subsequence1[index1] - subsequence2[index2];
+				if (difference != 0) {
+					return difference;
+				}
+				index1++;
+				index2++;
+			}
+			return (x - index1) - (y - index2);
+		}
 	}
 }
