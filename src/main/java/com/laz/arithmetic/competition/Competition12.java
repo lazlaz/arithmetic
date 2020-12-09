48e0bbd29620e20d855c0a74c65431e40a2e393f
zhouxingting@newrank.cn <zhouxingting@newrank.cn>
2020-12-09 14:12:54 +0800

    连接连续二进制数字

--- src/main/java/com/laz/arithmetic/competition/Competition12.java
+++ src/main/java/com/laz/arithmetic/competition/Competition12.java
@@ -44,25 +44,46 @@ public class Competition12 {
 	// K 和数对的最大数目
 	@Test
 	public void test2() {
-		Assert.assertEquals(2, maxOperations(new int[] {1,2,3,4},5));
-		
-		Assert.assertEquals(1, maxOperations(new int[] {3,1,3,4,3},6));
+		Assert.assertEquals(2, maxOperations(new int[] { 1, 2, 3, 4 }, 5));
+
+		Assert.assertEquals(1, maxOperations(new int[] { 3, 1, 3, 4, 3 }, 6));
 	}
 
 	public int maxOperations(int[] nums, int k) {
 		Arrays.sort(nums);
-		int p = 0,q=nums.length-1;
+		int p = 0, q = nums.length - 1;
 		int res = 0;
-		while (p<q) {
-			if (nums[p]+nums[q] == k) {
+		while (p < q) {
+			if (nums[p] + nums[q] == k) {
 				res++;
 				p++;
 				q--;
-			} else if (nums[p]+nums[q] > k) {
+			} else if (nums[p] + nums[q] > k) {
 				q--;
-			} else if (nums[p]+nums[q] < k) {
+			} else if (nums[p] + nums[q] < k) {
 				p++;
 			}
+		}
+		return res;
+	}
+
+	// 连接连续二进制数字
+	@Test
+	public void test3() {
+		Assert.assertEquals(1, concatenatedBinary(1));
+		Assert.assertEquals(27, concatenatedBinary(3));
+		Assert.assertEquals(505379714, concatenatedBinary(12));
+	}
+
+	public int concatenatedBinary(int n) {
+		final int MOD = 1000000007;
+		int res = 0,shift = 0;
+		for (int i=1;i<=n;i++) {
+			if ((i&(i-1))==0) {
+				//判断是否为2的冥 2的1次方，2次方等等
+				shift++;
+			}
+			res = (int) ((((long)res<<shift)+i)%MOD);
 		}
 		return res;
 	}
