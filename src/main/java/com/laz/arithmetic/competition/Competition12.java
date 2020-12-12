e8f82bffc3679c1568a9e5ead962a570cc99b870
zhouxingting@newrank.cn <zhouxingting@newrank.cn>
2020-12-12 22:27:22 +0800

    最小不兼容性

--- src/main/java/com/laz/arithmetic/competition/Competition12.java
+++ src/main/java/com/laz/arithmetic/competition/Competition12.java
@@ -1,6 +1,8 @@
 package com.laz.arithmetic.competition;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -77,14 +79,81 @@ public class Competition12 {
 
 	public int concatenatedBinary(int n) {
 		final int MOD = 1000000007;
-		int res = 0,shift = 0;
-		for (int i=1;i<=n;i++) {
-			if ((i&(i-1))==0) {
-				//判断是否为2的冥 2的1次方，2次方等等
+		int res = 0, shift = 0;
+		for (int i = 1; i <= n; i++) {
+			if ((i & (i - 1)) == 0) {
+				// 判断是否为2的冥 2的1次方，2次方等等
 				shift++;
 			}
-			res = (int) ((((long)res<<shift)+i)%MOD);
+			res = (int) ((((long) res << shift) + i) % MOD);
 		}
 		return res;
+	}
+
+	// 最小不兼容性 Dont't Know
+	@Test
+	public void test4() {
+		Assert.assertEquals(4, new Solution4().minimumIncompatibility(new int[] { 1, 2, 1, 4 }, 2));
+	}
+
+	// https://leetcode-cn.com/problems/minimum-incompatibility/solution/java-zhuang-tai-ya-suo-dp-dai-zhu-shi-by-fkig/
+	class Solution4 {
+		public int minimumIncompatibility(int[] nums, int k) {
+			int n = nums.length;
+			int m = n / k;
+			int[] dp = new int[1 << n];
+			int[] value = new int[1 << n];
+			Arrays.fill(value, -1);
+			Arrays.fill(dp, Integer.MAX_VALUE);
+			dp[0] = 0;
+			// 预处理value数组，value[j]代表 j这个集合的 不兼容性
+			for (int i = 0; i < (1 << n); i++) {
+				int count = countOne(i);// 计算二进制中1的数量
+				if (count == m) {// 条件1：子集中有m个1
+					// 条件2：每个数字只能出现一次
+					Map<Integer, Integer> map = new HashMap<>();// 来存子集中每个数字出现的次数
+					boolean flag = true;
+					int max = Integer.MIN_VALUE;
+					int min = Integer.MAX_VALUE;
+					for (int j = 0; j < n; j++) {
+						if ((i >> j & 1) == 1) { // i的第j位=1, 代表选了nums[j]
+							min = Math.min(min, nums[j]);
+							max = Math.max(max, nums[j]);
+							if (map.containsKey(nums[j])) {
+								flag = false;
+								break;
+							} else {
+								map.put(nums[j], 1);
+							}
+						}
+					}
+					// 如果这个子集合法
+					if (flag) {
+						value[i] = max - min;// 更新子集i的不兼容性
+					}
+				}
+			}
+			// 进行状态转移 ,dp[i]代表当选择的元素集合为i时最小的不兼容性的和
+			for (int i = 0; i < (1 << n); i++) {
+				if (countOne(i) % m == 0) {// 当前的选择方式组合mask是否有n/k的倍数个1
+					// 枚举最后一个选择的子集j
+					for (int j = i; j != 0; j = (j - 1) & i) {
+						if (value[j] != -1 && dp[i ^ j] != Integer.MAX_VALUE) { // i^j表示从i中去掉j这个集合
+							dp[i] = Math.min(dp[i], dp[i ^ j] + value[j]);
+						}
+					}
+				}
+			}
+			return dp[(1 << n) - 1] == Integer.MAX_VALUE ? -1 : dp[(1 << n) - 1];// 最后返回dp[1111111],表示每个数字都选了以后
+		}
+
+		public int countOne(int x) {
+			int res = 0;
+			while (x != 0) {
+				x &= (x - 1);
+				res++;
+			}
+			return res;
+		}
 	}
 }
