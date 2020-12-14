c540d43c0674c4d149c1ea313f98df8a41d6effb
zhouxingting@newrank.cn <zhouxingting@newrank.cn>
2020-12-14 12:46:57 +0800

    十-二进制数的最少数目

--- src/main/java/com/laz/arithmetic/competition/Competition13.java
+++ src/main/java/com/laz/arithmetic/competition/Competition13.java
@@ -14,15 +14,32 @@ public class Competition13 {
 
 	public int numberOfMatches(int n) {
 		int res = 0;
-		while (n>1) {
-			if (n%2==0) {
-				res += n/2;
-				n = n/2;
+		while (n > 1) {
+			if (n % 2 == 0) {
+				res += n / 2;
+				n = n / 2;
 			} else {
-				res += (n-1)/2;
+				res += (n - 1) / 2;
 				n = (n - 1) / 2 + 1;
 			}
 		}
 		return res;
+	}
+
+	// 十-二进制数的最少数目
+	@Test
+	public void test2() {
+		Assert.assertEquals(3, minPartitions("32"));
+		Assert.assertEquals(8, minPartitions("82734"));
+	}
+
+	public int minPartitions(String n) {
+		//规律，数字最大的数
+		int max = 0;
+		for (int i=0;i<n.length();i++) {
+			int v = n.charAt(i)-'0';
+			max = Math.max(v, max);
+		}
+		return max;
 	}
 }
