1c0be45a5351900b7c1e8922b9d9edee43d7dbe7
zhouxingting@newrank.cn <zhouxingting@newrank.cn>
2020-12-16 17:23:21 +0800

    石子游戏 VII

--- src/main/java/com/laz/arithmetic/competition/Competition13.java
+++ src/main/java/com/laz/arithmetic/competition/Competition13.java
@@ -34,12 +34,44 @@ public class Competition13 {
 	}
 
 	public int minPartitions(String n) {
-		//规律，数字最大的数
+		// 规律，数字最大的数
 		int max = 0;
-		for (int i=0;i<n.length();i++) {
-			int v = n.charAt(i)-'0';
+		for (int i = 0; i < n.length(); i++) {
+			int v = n.charAt(i) - '0';
 			max = Math.max(v, max);
 		}
 		return max;
 	}
+
+	// 石子游戏 VII
+	@Test
+	public void test3() {
+		Assert.assertEquals(122, stoneGameVII(new int[] { 7, 90, 5, 1, 100, 10, 10, 2 }));
+	}
+
+	//https://leetcode-cn.com/problems/stone-game-vii/solution/c-qu-jian-dp-si-lu-zheng-li-guo-cheng-by-3h0l/
+	public int stoneGameVII(int[] stones) {
+		int n = stones.length;
+		int[][] sum = new int[n][n];
+		for (int i=0;i<n;i++) {
+			for (int j=i;j<n;j++) {
+				if (i==j) {
+					sum[i][j] = stones[i];
+				} else {
+					sum[i][j] = stones[j]+sum[i][j-1];
+				}
+			}
+		}
+		int[][] dp = new int[n][n];
+		for (int i=n-1;i>=0;i--) {
+			for (int j=i+1;j<n;j++) {
+				if (j-i==1) {
+					dp[i][j] = Math.max(stones[i],stones[j]);
+				} else {
+					dp[i][j] = Math.max(sum[i+1][j]-dp[i+1][j], sum[i][j-1]-dp[i][j-1]);
+				}
+			}
+		}
+		return dp[0][n-1];
+ 	}
 }
