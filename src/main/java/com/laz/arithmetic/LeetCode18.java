cac49d25169b27cd75d31369f1993b557f652d46
liuzhai <578091472@qq.com>
2020-12-21 15:47:19 +0800

    使用最小花费爬楼梯

--- src/main/java/com/laz/arithmetic/LeetCode18.java
+++ src/main/java/com/laz/arithmetic/LeetCode18.java
@@ -568,4 +568,23 @@ public class LeetCode18 {
 		}
 		return stringBuilder.toString();
 	}
+
+	// 使用最小花费爬楼梯
+	@Test
+	public void test14() {
+		Assert.assertEquals(15, minCostClimbingStairs(new int[] { 10, 15, 20 }));
+		Assert.assertEquals(6, minCostClimbingStairs(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }));
+	}
+
+	public int minCostClimbingStairs(int[] cost) {
+		int n = cost.length;
+		int[] dp = new int[n+1];
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+		for (int i=2;i<n;i++) {
+			dp[i] = Math.min(dp[i-1]+cost[i], dp[i-2]+cost[i]);
+		}
+		dp[n] = Math.min(dp[n-1], dp[n-2]);
+		return dp[n];
+	}
 }
