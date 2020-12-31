0d031e70f942d9abc6a4997b587cbcc41f5aec02
liuzhai <578091472@qq.com>
2020-12-31 16:17:16 +0800

    剑指 Offer 44. 数字序列中某一位的数字

--- src/main/java/com/laz/arithmetic/swordoffer/Offer1.java
+++ src/main/java/com/laz/arithmetic/swordoffer/Offer1.java
@@ -839,4 +839,26 @@ public class Offer1 {
 			}
 		}
 	}
+
+	// 剑指 Offer 44. 数字序列中某一位的数字
+	@Test
+	public void test29() {
+		//Assert.assertEquals(3, findNthDigit(3));
+		Assert.assertEquals(4, findNthDigit(19));
+	}
+
+	// https://leetcode-cn.com/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof/solution/mian-shi-ti-44-shu-zi-xu-lie-zhong-mou-yi-wei-de-6/
+	public int findNthDigit(int n) {
+		int digit = 1;
+		long start = 1;
+		long count = 9;
+		while (n > count) { // 1.
+			n -= count;
+			digit += 1;
+			start *= 10;
+			count = digit * start * 9;
+		}
+		long num = start + (n - 1) / digit; // 2.
+		return Long.toString(num).charAt((n - 1) % digit) - '0'; // 3.
+	}
 }
