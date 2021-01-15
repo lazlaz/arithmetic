8de9b33f09378dfaec0d29df5cc71b4496e81ee5
laz <laz@WIN-C2CS6UB9L3K>
2021-01-15 20:06:32 +0800

    947. 移除最多的同行或同列石头

--- src/main/java/com/laz/arithmetic/LeetCode19.java
+++ src/main/java/com/laz/arithmetic/LeetCode19.java
@@ -499,4 +499,59 @@ public class LeetCode19 {
 		}
 		return list;
 	}
+	//947. 移除最多的同行或同列石头
+	@Test
+	public void test8() {
+		Assert.assertEquals(5, new Solution8().removeStones(new int[][] {
+			{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}
+		}));
+		
+		Assert.assertEquals(0, new Solution8().removeStones(new int[][] {
+			{0,0},{1,1}
+		}));
+	}
+	//https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/solution/947-yi-chu-zui-duo-de-tong-xing-huo-tong-ezha/
+	class Solution8{
+		public int removeStones(int[][] stones) {
+			UnionFind unionFind = new UnionFind();
+	        for (int[] stone : stones) {
+	            unionFind.union(stone[0] + 10001, stone[1]);
+	        }
+	        return stones.length - unionFind.getCount();
+
+		}
+		private class UnionFind {
+			private Map<Integer,Integer> parent;
+			private  int count;
+			public UnionFind() {
+				// TODO Auto-generated constructor stub
+				this.parent = new HashMap<Integer,Integer>();
+				this.count = 0;
+			}
+			public int getCount() {
+				return count;
+			}
+			public int find(int x) {
+				if (!parent.containsKey(x)) {
+					parent.put(x, x);
+					  // 并查集集中新加入一个结点，结点的父亲结点是它自己，所以连通分量的总数 +1
+					count++;
+				}
+				if (x!=parent.get(x)) {
+					 parent.put(x, find(parent.get(x)));
+				}
+				return parent.get(x);
+			}
+			public void union(int x,int y) {
+				int rootX = find(x);
+				int rootY = find(y);
+				if (rootX == rootY) {
+					return;
+				}
+				parent.put(rootX, rootY);
+				 // 两个连通分量合并成为一个，连通分量的总数 -1
+	            count--;
+			}
+		}
+	}
 }
