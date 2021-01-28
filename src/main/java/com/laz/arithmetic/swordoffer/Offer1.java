60c3e959ad9e52498320d946f98eac845f697663
liuzhai <578091472@qq.com>
2021-01-28 16:18:59 +0800

    剑指 Offer 66. 构建乘积数组

--- src/main/java/com/laz/arithmetic/swordoffer/Offer1.java
+++ src/main/java/com/laz/arithmetic/swordoffer/Offer1.java
@@ -1504,4 +1504,27 @@ public class Offer1 {
 		boolean x = n > 1 && (n += sumNums(n - 1)) > 0;
 		return n;
 	}
+
+	// 剑指 Offer 66. 构建乘积数组
+	@Test
+	public void test50() {
+		Assert.assertArrayEquals(new int[] { 120, 60, 40, 30, 24 }, constructArr(new int[] { 1, 2, 3, 4, 5 }));
+	}
+
+	// https://leetcode-cn.com/problems/gou-jian-cheng-ji-shu-zu-lcof/solution/mian-shi-ti-66-gou-jian-cheng-ji-shu-zu-biao-ge-fe/
+	public int[] constructArr(int[] a) {
+		if (a.length == 0)
+			return new int[0];
+		int[] b = new int[a.length];
+		b[0] = 1;
+		int tmp = 1;
+		for (int i = 1; i < a.length; i++) {
+			b[i] = b[i - 1] * a[i - 1];
+		}
+		for (int i = a.length - 2; i >= 0; i--) {
+			tmp *= a[i + 1];
+			b[i] *= tmp;
+		}
+		return b;
+	}
 }
