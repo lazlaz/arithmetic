84b245db7d8f5a79b8fd9edd9bb7a39b577c4c97
liuzhai <578091472@qq.com>
2021-02-22 10:31:55 +0800

    766. 托普利茨矩阵

--- src/main/java/com/laz/arithmetic/LeetCode20.java
+++ src/main/java/com/laz/arithmetic/LeetCode20.java
@@ -587,4 +587,46 @@ public class LeetCode20 {
 			list.add(index, v);
 		}
 	}
+
+	// 766. 托普利茨矩阵
+	@Test
+	public void test12() {
+//		Assert.assertEquals(true, isToeplitzMatrix(new int[][] { { 1, 2, 3, 4 }, { 5, 1, 2, 3 }, { 9, 5, 1, 2 } }));
+//		
+//		Assert.assertEquals(false, isToeplitzMatrix(new int[][] { { 1, 2 }, { 2,2 }}));
+		
+		Assert.assertEquals(true, isToeplitzMatrix(new int[][] { { 18 }, { 66 }}));
+	}
+
+	public boolean isToeplitzMatrix(int[][] matrix) {
+		int m = matrix.length;
+		int n = matrix[0].length;
+		
+		for (int i=0;i<m;i++) {
+			int v = matrix[i][0];
+			int y = i+1;
+			int x = 0+1;
+			while (x<n&&y<m) {
+				if (matrix[y][x]!=v) {
+					return false;
+				}
+				y++;
+				x++;
+			}
+		}
+		
+		for (int i=0;i<n;i++) {
+			int v = matrix[0][i];
+			int x = i+1;
+			int y = 0+1;
+			while (x<n&&y<m) {
+				if (matrix[y][x]!=v) {
+					return false;
+				}
+				y++;
+				x++;
+			}
+		}
+		return true;
+	}
 }
