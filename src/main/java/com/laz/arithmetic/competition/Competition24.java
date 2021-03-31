aab7c1b1168858d00dde8e17157cf139ec77b64d
liuzhai <578091472@qq.com>
2021-03-31 11:59:49 +0800

    1807. 替换字符串中的括号内容

--- src/main/java/com/laz/arithmetic/competition/Competition24.java
+++ src/main/java/com/laz/arithmetic/competition/Competition24.java
@@ -1,8 +1,13 @@
 package com.laz.arithmetic.competition;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 //https://leetcode-cn.com/contest/weekly-contest-234/
 public class Competition24 {
@@ -29,5 +34,42 @@ public class Competition24 {
     		}
     	}
     	return ans;
+    }
+    
+    //1807. 替换字符串中的括号内容
+    @Test
+    public void test3() {
+    	List<List<String>> knowledge = new ArrayList<>();
+    	knowledge.add(Arrays.asList("name","bob"));
+    	knowledge.add(Arrays.asList("age","two"));
+    	Assert.assertEquals("bobistwoyearsold", new Solution3().evaluate("(name)is(age)yearsold", knowledge));
+    }
+    class Solution3 {
+        public String evaluate(String s, List<List<String>> knowledge) {
+        	//knowledge转map
+        	Map<String,String> map = new HashMap<>();
+        	for (List<String> know:knowledge) {
+        		map.put(know.get(0), know.get(1));
+        	}
+        	//替换字符串
+        	StringBuilder sb = new StringBuilder();
+        	for (int i=0;i<s.length();i++) {
+        		if (s.charAt(i) == '(') {
+        			StringBuilder key = new StringBuilder();
+        			for (int j=i+1;j<s.length();j++) {
+        				if (s.charAt(j)==')') {
+        					i=j;
+        					break;
+        				}
+        				key.append(s.charAt(j));
+        			}
+        			String v = map.getOrDefault(key.toString(), "?");
+        			sb.append(v);
+        		} else {
+        			sb.append(s.charAt(i));
+        		}
+        	}
+        	return sb.toString();
+        }
     }
 }
