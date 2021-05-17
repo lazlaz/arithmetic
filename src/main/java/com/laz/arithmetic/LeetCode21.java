b172ae72994aee119fff5ceb9ada4fce811f8c57
liuzhai <578091472@qq.com>
2021-05-17 11:22:57 +0800

    993. 二叉树的堂兄弟节点

--- src/main/java/com/laz/arithmetic/LeetCode21.java
+++ src/main/java/com/laz/arithmetic/LeetCode21.java
@@ -678,4 +678,61 @@ public class LeetCode21 {
 	    	return dp[steps][0];
 	    }
 	}
+	
+	//993. 二叉树的堂兄弟节点
+	@Test
+	public void test19() {
+		TreeNode root = Utils.createTree(new Integer[] {1,2,3,null,4,null,5});
+		Assert.assertEquals(true, new Solution19().isCousins(root, 5, 4));
+	}
+	//https://leetcode-cn.com/problems/cousins-in-binary-tree/solution/er-cha-shu-de-tang-xiong-di-jie-dian-by-mfh2d/
+	class Solution19 {
+		// x 的信息
+	    int x;
+	    TreeNode xParent;
+	    int xDepth;
+	    boolean xFound = false;
+
+	    // y 的信息
+	    int y;
+	    TreeNode yParent;
+	    int yDepth;
+	    boolean yFound = false;
+	    public boolean isCousins(TreeNode root, int x, int y) {
+	        this.x = x;
+	        this.y = y;
+	        dfs(root, 0, null);
+	        return xDepth == yDepth && xParent != yParent;
+	    }
+
+	    public void dfs(TreeNode node, int depth, TreeNode parent) {
+	        if (node == null) {
+	            return;
+	        }
+	        if (node.val == x) {
+	            xParent = parent;
+	            xDepth = depth;
+	            xFound = true;
+	        } else if (node.val == y) {
+	            yParent = parent;
+	            yDepth = depth;
+	            yFound = true;
+	        }
+
+	        // 如果两个节点都找到了，就可以提前退出遍历
+	        // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+	        if (xFound && yFound) {
+	            return;
+	        }
+
+	        dfs(node.left, depth + 1, node);
+
+	        if (xFound && yFound) {
+	            return;
+	        }
+
+	        dfs(node.right, depth + 1, node);
+	    }
+
+	}
 }
