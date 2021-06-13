a263af02becffecdaf2c10eb8b5118a634088d12
laz <578091472@qq.com>
2021-06-13 09:05:08 +0800

    1449. 数位成本和为目标值的最大数字

--- src/main/java/com/laz/arithmetic/LeetCode22.java
+++ src/main/java/com/laz/arithmetic/LeetCode22.java
@@ -1,6 +1,7 @@
 package com.laz.arithmetic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
@@ -34,9 +35,9 @@ public class LeetCode22 {
 			for (int i = 1; i <= m; ++i) {
 				for (int j = 1; j <= n; ++j) {
 					pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
-					if (queue.size()<k) {
+					if (queue.size() < k) {
 						queue.add(pre[i][j]);
-					} else if (queue.peek()<pre[i][j]) {
+					} else if (queue.peek() < pre[i][j]) {
 						queue.poll();
 						queue.add(pre[i][j]);
 					}
@@ -45,84 +46,82 @@ public class LeetCode22 {
 			return queue.peek();
 		}
 	}
-	
-	//692. 前K个高频单词
+
+	// 692. 前K个高频单词
 	@Test
 	public void test2() {
-		Assert.assertEquals("i,love", Joiner.on(",").join(new Solution2().topKFrequent(new String[] {
-				"i", "love", "leetcode", "i", "love", "coding"
-		}, 2)));
+		Assert.assertEquals("i,love", Joiner.on(",").join(
+				new Solution2().topKFrequent(new String[] { "i", "love", "leetcode", "i", "love", "coding" }, 2)));
 	}
+
 	class Solution2 {
 		class Word {
 			String word;
 			int count;
 		}
-	    public List<String> topKFrequent(String[] words, int k) {
-	    	Map<String,Integer> wordMap = new HashMap<String,Integer>();
-	    	for (int i=0;i<words.length;i++) {
-	    		wordMap.put(words[i], wordMap.getOrDefault(words[i], 0)+1);
-	    	}
-	    	PriorityQueue<Word> wordStack = new PriorityQueue<Word>(new Comparator<Word>() {
+
+		public List<String> topKFrequent(String[] words, int k) {
+			Map<String, Integer> wordMap = new HashMap<String, Integer>();
+			for (int i = 0; i < words.length; i++) {
+				wordMap.put(words[i], wordMap.getOrDefault(words[i], 0) + 1);
+			}
+			PriorityQueue<Word> wordStack = new PriorityQueue<Word>(new Comparator<Word>() {
 
 				@Override
 				public int compare(Word o1, Word o2) {
-					if (o1.count>o2.count) {
+					if (o1.count > o2.count) {
 						return -1;
 					}
-					if (o1.count<o2.count) {
+					if (o1.count < o2.count) {
 						return 1;
 					}
 					return o1.word.compareTo(o2.word);
 				}
 			});
-	    	for (Map.Entry<String,Integer> entry : wordMap.entrySet()) {
+			for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
 				Word w = new Word();
 				w.count = entry.getValue();
 				w.word = entry.getKey();
 				wordStack.add(w);
 			}
-	    	List<String> res = new ArrayList<>();
-	    	int index = 0;
-	    	while (!wordStack.isEmpty() && index<k) {
-	    		res.add(wordStack.poll().word);
-	    		index++;
-	    	}
-	    	return res;
-	    }
+			List<String> res = new ArrayList<>();
+			int index = 0;
+			while (!wordStack.isEmpty() && index < k) {
+				res.add(wordStack.poll().word);
+				index++;
+			}
+			return res;
+		}
 	}
-	
-	//1035. 不相交的线
+
+	// 1035. 不相交的线
 	@Test
 	public void test3() {
-		Assert.assertEquals(2, new Solution3().maxUncrossedLines(new int[] {
-				1,4,2
-		}, new int[] {
-				1,2,4
-		}));
+		Assert.assertEquals(2, new Solution3().maxUncrossedLines(new int[] { 1, 4, 2 }, new int[] { 1, 2, 4 }));
 	}
-	//https://leetcode-cn.com/problems/uncrossed-lines/solution/bu-xiang-jiao-de-xian-by-leetcode-soluti-6tqz/
+
+	// https://leetcode-cn.com/problems/uncrossed-lines/solution/bu-xiang-jiao-de-xian-by-leetcode-soluti-6tqz/
 	class Solution3 {
-	    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-	    	//最长公共子序列
-	    	int m = nums1.length,n = nums2.length;
-	    	int[][]dp = new int[m+1][n+1];
-	    	for (int i=1;i<=m;i++) {
-	    		int num1 = nums1[i-1];
-	    		for (int j=1;j<=n;j++) {
-	    			int num2 = nums2[j-1];
-	    			if (num1 == num2) {
-	    				dp[i][j] = dp[i-1][j-1]+1;
-	    			} else {
-	    				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-	    			}
-	    		}
-	    	}
-	    	return dp[m][n];
-	    }
+		public int maxUncrossedLines(int[] nums1, int[] nums2) {
+			// 最长公共子序列
+			int m = nums1.length, n = nums2.length;
+			int[][] dp = new int[m + 1][n + 1];
+			for (int i = 1; i <= m; i++) {
+				int num1 = nums1[i - 1];
+				for (int j = 1; j <= n; j++) {
+					int num2 = nums2[j - 1];
+					if (num1 == num2) {
+						dp[i][j] = dp[i - 1][j - 1] + 1;
+					} else {
+						dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+					}
+				}
+			}
+			return dp[m][n];
+		}
 	}
-	
-	//1190. 反转每对括号间的子串
+
+	// 1190. 反转每对括号间的子串
 	@Test
 	public void test4() {
 		Assert.assertEquals("iloveu", new Solution4().reverseParentheses("(u(love)i)"));
@@ -130,150 +129,191 @@ public class LeetCode22 {
 		Assert.assertEquals("leetcode", new Solution4().reverseParentheses("(ed(et(oc))el)"));
 		Assert.assertEquals("apmnolkjihgfedcbq", new Solution4().reverseParentheses("a(bcdefghijkl(mno)p)q"));
 	}
+
 	class Solution4 {
-	    public String reverseParentheses(String s) {
-	    	if (s.length()==0) {
-	    		return s;
-	    	}
-	    	char[] chars = s.toCharArray();
-	    	Deque<StringBuilder> stack = new LinkedList<>();
-	    	StringBuilder sb = new StringBuilder();
-	    	for (int i=0;i<chars.length;i++) {
-	    		char c = chars[i];
-	    		if (c == '(') {
-	    			stack.push(new StringBuilder(sb));
-	    			sb = new StringBuilder();
-	    		} else if (c == ')') {
-	    			StringBuilder sb2 = stack.pop();
-	    			sb2.append(sb.reverse().toString());
-	    			sb = sb2;
-	    		} else {
-	    			sb.append(c);
-	    		}
-	    	}
-	    	return sb.toString();
-	    }
+		public String reverseParentheses(String s) {
+			if (s.length() == 0) {
+				return s;
+			}
+			char[] chars = s.toCharArray();
+			Deque<StringBuilder> stack = new LinkedList<>();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < chars.length; i++) {
+				char c = chars[i];
+				if (c == '(') {
+					stack.push(new StringBuilder(sb));
+					sb = new StringBuilder();
+				} else if (c == ')') {
+					StringBuilder sb2 = stack.pop();
+					sb2.append(sb.reverse().toString());
+					sb = sb2;
+				} else {
+					sb.append(c);
+				}
+			}
+			return sb.toString();
+		}
 	}
-	//477. 汉明距离总和
+
+	// 477. 汉明距离总和
 	@Test
 	public void test5() {
-		Assert.assertEquals(6, new Solution5().totalHammingDistance(new int[] {
-				4, 14, 2
-		}));
+		Assert.assertEquals(6, new Solution5().totalHammingDistance(new int[] { 4, 14, 2 }));
 	}
-	//https://leetcode-cn.com/problems/total-hamming-distance/solution/yi-ming-ju-chi-zong-he-by-leetcode-solut-t0ev/
-	class Solution5 {
-	    public int totalHammingDistance(int[] nums) {
-	    	  int ans = 0, n = nums.length;
-	          for (int i = 0; i < 30; ++i) {
-	              int c = 0;
-	              for (int val : nums) {
-	                  c += (val >> i) & 1;
-	              }
-	              ans += c * (n - c);
-	          }
-	          return ans;
 
-	    }
+	// https://leetcode-cn.com/problems/total-hamming-distance/solution/yi-ming-ju-chi-zong-he-by-leetcode-solut-t0ev/
+	class Solution5 {
+		public int totalHammingDistance(int[] nums) {
+			int ans = 0, n = nums.length;
+			for (int i = 0; i < 30; ++i) {
+				int c = 0;
+				for (int val : nums) {
+					c += (val >> i) & 1;
+				}
+				ans += c * (n - c);
+			}
+			return ans;
+
+		}
 	}
-	
-	//1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？
+
+	// 1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？
 	@Test
 	public void test6() {
-		Assert.assertArrayEquals(new boolean[] {
-				true,false,true
-		}, new Solution6().canEat(new int[] {
-				7,4,5,3,8
-		}, new int[][] {
-			{0,2,2},{4,2,4},{2,13,1000000000}
-		}));
+		Assert.assertArrayEquals(new boolean[] { true, false, true }, new Solution6()
+				.canEat(new int[] { 7, 4, 5, 3, 8 }, new int[][] { { 0, 2, 2 }, { 4, 2, 4 }, { 2, 13, 1000000000 } }));
 	}
+
 	class Solution6 {
-	    public boolean[] canEat(int[] candiesCount, int[][] queries) {
-	    	int  n = candiesCount.length;
-	    	//前缀和 用long 范围
-	    	long[] pre = new long[n+1]; 
-	    	for (int i=1;i<=n;i++) {
-	    		pre[i] = pre[i-1]+candiesCount[i-1];
-	    	}
-	    	int len = queries.length;
-	    	boolean[] ans = new boolean[len];
-	    	for (int i=0;i<len;i++) {
-	    		int[] query = queries[i];
-	    		int favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
-	    		//求query的区间
-	    		long x1 = favoriteDay +1;
-	    		long y1 = (long)(favoriteDay+1)*dailyCap;
-	    		
-	    		//能够吃到i类糖果的区间
-	    		long x2 = favoriteType ==0?1:pre[favoriteType]+1;
-	    		long y2 = pre[favoriteType+1];
-	    		ans[i] = !(x1>y2||y1<x2);
-	    	}
-	    	return ans;
-	    }
+		public boolean[] canEat(int[] candiesCount, int[][] queries) {
+			int n = candiesCount.length;
+			// 前缀和 用long 范围
+			long[] pre = new long[n + 1];
+			for (int i = 1; i <= n; i++) {
+				pre[i] = pre[i - 1] + candiesCount[i - 1];
+			}
+			int len = queries.length;
+			boolean[] ans = new boolean[len];
+			for (int i = 0; i < len; i++) {
+				int[] query = queries[i];
+				int favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
+				// 求query的区间
+				long x1 = favoriteDay + 1;
+				long y1 = (long) (favoriteDay + 1) * dailyCap;
+
+				// 能够吃到i类糖果的区间
+				long x2 = favoriteType == 0 ? 1 : pre[favoriteType] + 1;
+				long y2 = pre[favoriteType + 1];
+				ans[i] = !(x1 > y2 || y1 < x2);
+			}
+			return ans;
+		}
 	}
-	
-	//523. 连续的子数组和
+
+	// 523. 连续的子数组和
 	@Test
 	public void test7() {
 //		Assert.assertEquals(true, new Solution7().checkSubarraySum(new int[] {
 //				23,2,4,6,7
 //		}, 6));
-		Assert.assertEquals(true, new Solution7().checkSubarraySum(new int[] {
-				5,0,0,0
-		}, 3));
+		Assert.assertEquals(true, new Solution7().checkSubarraySum(new int[] { 5, 0, 0, 0 }, 3));
 	}
+
 	class Solution7 {
-	    public boolean checkSubarraySum(int[] nums, int k) {
-	    		//提示：要使得两者除 k相减为整数，需要满足 sum[j] 和 sum[i - 1] 对 k取余相同。
-	    		int n = nums.length;
-	    		if (n<2) {
-	    			return false;
-	    		}
-	    		int[] preSum = new int[n+1];
-	    		for (int i=1;i<preSum.length;i++) {
-	    			preSum[i] = preSum[i-1]+nums[i-1];
-	    		}
-	    		Set<Integer> set = new HashSet<>();
-	    		for (int i=2;i<preSum.length;i++) {
-	    			set.add(preSum[i-2]%k); //添加当前值左移2的位的值，因为不少于2 的长度
-	    			if (set.contains(preSum[i]%k)) {
-	    				return true;
-	    			}
-	    		}
-	    		return false;
-	    }
+		public boolean checkSubarraySum(int[] nums, int k) {
+			// 提示：要使得两者除 k相减为整数，需要满足 sum[j] 和 sum[i - 1] 对 k取余相同。
+			int n = nums.length;
+			if (n < 2) {
+				return false;
+			}
+			int[] preSum = new int[n + 1];
+			for (int i = 1; i < preSum.length; i++) {
+				preSum[i] = preSum[i - 1] + nums[i - 1];
+			}
+			Set<Integer> set = new HashSet<>();
+			for (int i = 2; i < preSum.length; i++) {
+				set.add(preSum[i - 2] % k); // 添加当前值左移2的位的值，因为不少于2 的长度
+				if (set.contains(preSum[i] % k)) {
+					return true;
+				}
+			}
+			return false;
+		}
 	}
-	
-	//518. 零钱兑换 II
+
+	// 518. 零钱兑换 II
 	@Test
 	public void test8() {
-		Assert.assertEquals(4, new Solution8().change(5, new int[] {
-				1,2,5
-		}));
-		
-		Assert.assertEquals(0, new Solution8().change(3, new int[] {
-				2
-		}));
-		
-		Assert.assertEquals(1, new Solution8().change(0, new int[] {
-				7
-		}));
+		Assert.assertEquals(4, new Solution8().change(5, new int[] { 1, 2, 5 }));
+
+		Assert.assertEquals(0, new Solution8().change(3, new int[] { 2 }));
+
+		Assert.assertEquals(1, new Solution8().change(0, new int[] { 7 }));
 	}
-	//https://leetcode-cn.com/problems/coin-change-2/solution/gong-shui-san-xie-xiang-jie-wan-quan-bei-6hxv/
+
+	// https://leetcode-cn.com/problems/coin-change-2/solution/gong-shui-san-xie-xiang-jie-wan-quan-bei-6hxv/
 	class Solution8 {
 		public int change(int cnt, int[] cs) {
 			int n = cs.length;
-	        int[] f = new int[cnt + 1]; 
-	        f[0] = 1;
-	        for (int i = 1; i <= n; i++) {
-	            int val = cs[i - 1];
-	            for (int j = val; j <= cnt; j++) {
-	                f[j] += f[j - val];
-	            }
-	        }
-	        return f[cnt];
+			int[] f = new int[cnt + 1];
+			f[0] = 1;
+			for (int i = 1; i <= n; i++) {
+				int val = cs[i - 1];
+				for (int j = val; j <= cnt; j++) {
+					f[j] += f[j - val];
+				}
+			}
+			return f[cnt];
+		}
+	}
+
+	// 1449. 数位成本和为目标值的最大数字
+	@Test
+	public void test9() {
+		Assert.assertEquals("7772", new Solution9().largestNumber(new int[] { 4, 3, 2, 5, 6, 7, 2, 5, 5 }, 9));
+	}
+
+	// https://leetcode-cn.com/problems/form-largest-integer-with-digits-that-add-up-to-target/solution/shu-wei-cheng-ben-he-wei-mu-biao-zhi-de-dnh86/
+	class Solution9 {
+		public String largestNumber(int[] cost, int target) {
+			int[][] dp = new int[10][target + 1]; // 选择前i个cost，成本为j时最大的位数
+			for (int i = 0; i < 10; ++i) {
+				Arrays.fill(dp[i], Integer.MIN_VALUE);
+			}
+			int[][] from = new int[10][target + 1];
+			dp[0][0] = 0;
+			for (int i = 0; i < 9; ++i) {
+				int c = cost[i];
+				for (int j = 0; j <= target; ++j) {
+					if (j < c) {
+						dp[i + 1][j] = dp[i][j];
+						from[i + 1][j] = j;
+					} else {
+						if (dp[i][j] > dp[i + 1][j - c] + 1) {
+							dp[i + 1][j] = dp[i][j];
+							from[i + 1][j] = j;
+						} else {
+							dp[i + 1][j] = dp[i + 1][j - c] + 1;
+							from[i + 1][j] = j - c;
+						}
+					}
+				}
+			}
+			if (dp[9][target] < 0) {
+				return "0";
+			}
+			StringBuffer sb = new StringBuffer();
+			int i = 9, j = target;
+			while (i > 0) {
+				if (j == from[i][j]) {
+					//说明该位置没有被选中
+					--i;
+				} else {
+					sb.append(i);
+					j = from[i][j];
+				}
+			}
+			return sb.toString();
 		}
 	}
 }
