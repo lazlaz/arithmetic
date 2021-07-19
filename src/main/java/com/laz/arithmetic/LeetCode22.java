a1be7281fa18cc3ea5b34daed113a3d6f6e9fa68
lliu1 <lliu1@10.109.11.220>
2021-07-19 17:03:24 +0800

    1838. 最高频元素的频数

--- src/main/java/com/laz/arithmetic/LeetCode22.java
+++ src/main/java/com/laz/arithmetic/LeetCode22.java
@@ -643,4 +643,32 @@ public class LeetCode22 {
 			return new String(cs);
 		}
 	}
+	
+	//1838. 最高频元素的频数
+	@Test
+	public void test17() {
+		Assert.assertEquals(3, new Solution17().maxFrequency(new int[] {
+				1,2,4
+		}, 5));
+	}
+	
+	class Solution17 {
+	    public int maxFrequency(int[] nums, int k) {
+	    	//先排序
+	    	Arrays.sort(nums);
+	    	int n = nums.length;
+	    	long total = 0;
+	    	int left = 0, res = 1;
+	    	for (int right=1;right<n;++right) {
+	    		//如果不考虑次数，左边界到右边界的次数,累加
+	    		total += (long)(nums[right]-nums[right-1])*(right-left);
+	    		while (total > k) {
+	    			total -= nums[right]-nums[left];
+	    			++left;
+	    		}
+	    		res = Math.max(res, right-left+1);
+	    	}
+	    	return res;
+	    }
+	}
 }
