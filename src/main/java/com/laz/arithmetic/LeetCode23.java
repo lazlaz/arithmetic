c224013be6d60c774a7fb6e662f679f8d27e7b25
laz <578091472@qq.com>
2022-04-20 21:43:09 +0800

    388. 文件的最长绝对路径

--- src/main/java/com/laz/arithmetic/LeetCode23.java
+++ src/main/java/com/laz/arithmetic/LeetCode23.java
@@ -686,4 +686,54 @@ public class LeetCode23 {
             return res;
         }
     }
+
+    //388. 文件的最长绝对路径
+    @Test
+    public void test13() {
+        Solution13 solution13 = new Solution13();
+        Assert.assertEquals(20, solution13.lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"));
+    }
+
+    class Solution13 {
+        public int lengthLongestPath(String input) {
+            int n = input.length();
+            int pos = 0;
+            int ans = 0;
+            Deque<Integer> stack = new ArrayDeque<Integer>();
+
+            while (pos < n) {
+                /* 检测当前文件的深度 */
+                int depth = 1;
+                while (pos < n && input.charAt(pos) == '\t') {
+                    pos++;
+                    depth++;
+                }
+                /* 统计当前文件名的长度 */
+                boolean isFile = false;
+                int len = 0;
+                while (pos < n && input.charAt(pos) != '\n') {
+                    if (input.charAt(pos) == '.') {
+                        isFile = true;
+                    }
+                    len++;
+                    pos++;
+                }
+                /* 跳过当前的换行符 */
+                pos++;
+
+                while (stack.size() >= depth) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    len += stack.peek() + 1;
+                }
+                if (isFile) {
+                    ans = Math.max(ans, len);
+                } else {
+                    stack.push(len);
+                }
+            }
+            return ans;
+        }
+    }
 }
