4f59ffd5f3ae3f0cacb0a67273b3de56bdb4b07d
laz <578091472@qq.com>
2022-06-08 21:01:51 +0800

    1037. 有效的回旋镖

--- src/main/java/com/laz/arithmetic/LeetCode24.java
+++ src/main/java/com/laz/arithmetic/LeetCode24.java
@@ -37,4 +37,21 @@ public class LeetCode24 {
             dfs(root.right);
         }
     }
+
+    //1037. 有效的回旋镖
+    @Test
+    public void test2() {
+        Solution2 solution2 = new Solution2();
+        solution2.isBoomerang(new int[][]{
+                {1,1},{2,3},{3,2}
+        });
+    }
+
+    class Solution2 {
+        public boolean isBoomerang(int[][] points) {
+            int[] v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
+            int[] v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
+            return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+        }
+    }
 }
