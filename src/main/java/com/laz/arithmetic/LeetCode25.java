0b37a4b2fa024de0564f2b2f3353ced2e9f0e194
zxt <1404203081@qq.com>
2023-07-19 20:56:39 +0800

    874. 模拟行走机器人

--- src/main/java/com/laz/arithmetic/LeetCode25.java
+++ src/main/java/com/laz/arithmetic/LeetCode25.java
@@ -3,6 +3,8 @@ package com.laz.arithmetic;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.*;
+
 public class LeetCode25 {
     @Test
     //1911. 最大子序列交替和
@@ -26,6 +28,65 @@ public class LeetCode25 {
                 odd[i] = Math.max(odd[i-1], even[i-1]-nums[i]);
             }
             return Math.max(even[n-1], odd[n-1]);
+        }
+    }
+
+    //874. 模拟行走机器人
+    @Test
+    public void test2() {
+        Solution2 solution2 = new Solution2();
+        Assert.assertEquals(solution2.robotSim(new int[] {
+                4,-1,4,-2,4
+        }, new int[][]{
+                {2,4}
+        }), 65);
+
+        Assert.assertEquals(solution2.robotSim(new int[] {
+              7,-2,-2,7,5
+        }, new int[][]{
+                {-3,2},{-2,1},{0,1},{-2,4},{-1,0},{-2,-3},{0,-3},{4,4},{-3,3},{2,2}
+        }), 4);
+//
+        Assert.assertEquals(solution2.robotSim(new int[] {
+               2,-1,8,-1,6
+        }, new int[][]{
+                {1,5},{-5,-5},{0,4},{-1,-1},{4,5},{-5,-3},{-2,1},{0,5},{0,-1}
+        }), 80);
+        Assert.assertEquals(solution2.robotSim(new int[] {
+              -2,3,-1,-2,4
+        }, new int[][]{
+                {-2,2},{4,-3},{2,3},{-3,-2},{-2,-4},{0,-4},{2,2},{3,-2},{2,-5},{2,1}
+        }), 49);
+    }
+
+    class Solution2 {
+        public int robotSim(int[] commands, int[][] obstacles) {
+            int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+            int px = 0, py = 0, d = 1;
+            Set<Integer> set = new HashSet<Integer>();
+            for (int[] obstacle : obstacles) {
+                set.add(obstacle[0] * 60001 + obstacle[1]);
+            }
+            int res = 0;
+            for (int c : commands) {
+                if (c < 0) {
+                    d += c == -1 ? 1 : -1;
+                    d %= 4;
+                    if (d < 0) {
+                        d += 4;
+                    }
+                } else {
+                    for (int i = 0; i < c; i++) {
+                        if (set.contains((px + dirs[d][0]) * 60001 + py + dirs[d][1])) {
+                            break;
+                        }
+                        px += dirs[d][0];
+                        py += dirs[d][1];
+                        res = Math.max(res, px * px + py * py);
+                    }
+                }
+            }
+            return res;
         }
     }
 }
